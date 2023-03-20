8da3caa56b3140d2268856c359e6c0b44b7ca2b7
Nazareno Valentini <nazareno.valentini@gmail.com>
2023-03-20 18:55:11 +0100

    update Text test

--- front/src/__tests__/atoms/Text.test.tsx
+++ front/src/__tests__/atoms/Text.test.tsx
@@ -3,7 +3,13 @@ import { Text } from '../../components/atoms'
 
 describe('Text', () => {
   it('should render the text', () => {
-    render(<Text>test</Text>)
+    render(
+      <Text color="red" fontSize="50px">
+        test
+      </Text>
+    )
     expect(screen.getByText('test')).toBeInTheDocument()
+    expect(screen.getByText('test')).toHaveStyle({ color: 'red' })
+    expect(screen.getByText('test')).toHaveStyle({ 'font-size': '50px' })
   })
 })
