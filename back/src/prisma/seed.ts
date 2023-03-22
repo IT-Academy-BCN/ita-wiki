27331363aa1c5741aa218293c0a6076bc745edb4
Iván Legrán <68439095+ivanlegranbizarro@users.noreply.github.com>
2023-03-22 15:15:22 +0100

    finalizando
    aplicando validaciones en el seed
    
    dni added to model and seed
    
    validaciones para DNI y User

--- back/src/prisma/seed.ts
+++ back/src/prisma/seed.ts
@@ -1,15 +1,21 @@
 import { PrismaClient } from '@prisma/client'
+import { UserSchema } from '../schemas/UserSchema';
 
 const prisma = new PrismaClient()
 
 async function main() {
+  const data = {
+    email: 'test@example.com',
+    password: 'password',
+    name: 'Test User',
+    dni: '45632452a',
+    status: 'ACTIVE',
+  }
+
+  const validatedData = UserSchema.parse(data)
+
   const user = await prisma.user.create({
-    data: {
-      email: 'test@example.com',
-      password: 'password',
-      name: 'Test User',
-      status: 'ACTIVE',
-    },
+    data: validatedData,
   })
   console.log(user)
 }
---
+++ back/src/schemas/DNISchema.ts
@@ -0,0 +1,19 @@
+import { z } from "zod";
+
+const validNIEPrefixes = ["X", "Y", "Z"];
+const validDNIPrefixes = [...Array(23).keys()].map((i) =>
+  (i + 1).toString()
+);
+
+const DNI_REGEX = /^(([XYZ]\d{7,8})|(\d{8}))([a-zA-Z])$/i;
+
+export const DNISchema = z.string()
+  .regex(DNI_REGEX)
+  .transform((value) => value.toUpperCase())
+  .refine((value) => {
+    const firstLetter = value.charAt(0);
+    return (
+      validDNIPrefixes.includes(firstLetter) ||
+      validNIEPrefixes.includes(firstLetter)
+    );
+  });
