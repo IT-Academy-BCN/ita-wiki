a1e6a46e89c6c1705f21e56ab5c7d6c048aee672
Kevin Mamaqi <kevinmamaqi@gmail.com>
2023-03-22 09:34:45 +0100

    update

--- front/src/components/atoms/Title.ts
+++ front/src/components/atoms/Title.ts
@@ -2,17 +2,17 @@ import { HTMLAttributes } from 'react'
 import styled from 'styled-components'
 import { colors } from '../../styles'
 import font from '../../styles/font'
+import Text from './Text'
 
 type TTitle = HTMLAttributes<HTMLHeadingElement> & {
   as: 'h1' | 'h2' | 'h3'
-  fontWeight?: 'normal' | 'bold'
-  fontFamily?: string
 }
 
-const Title = styled.h1<TTitle>`
+const Title = styled(Text).attrs<TTitle>((props) => ({
+  as: props?.as || 'h1',
+}))<TTitle>`
   color: ${({ color }) => color || colors.black.black2};
-  font-weight: ${({ fontWeight }) => fontWeight || 'normal'};
-  font-family: ${({ fontFamily }) => fontFamily || font.fontFamily};
+  font-family: ${font.fontFamily};
   ${({ as }) => as === 'h1' && `font-size: ${font.h1}`};
   ${({ as }) => as === 'h2' && `font-size: ${font.h2}`};
   ${({ as }) => as === 'h3' && `font-size: ${font.h3}`};
