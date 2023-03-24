410f46b5a0e308e20eb2ce880018503b2fe1f4e3
Sergio Paniagua Sánchez <97700906+Spanioulis@users.noreply.github.com>
2023-03-24 11:23:31 +0100

    remove // @ts-nocheck and update

--- front/src/utils/validateID.ts
+++ front/src/utils/validateID.ts
@@ -1,23 +1,25 @@
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-nocheck
-
 // NIEs validation included
 export function validateID(dni: string) {
-  let number
-  let letter
+  let number: number
+  let letter: string
   const expressionRegularDNI = /^[XYZ]?\d{5,8}[A-Z]$/
 
   const DNI = dni.toUpperCase()
 
   if (expressionRegularDNI.test(DNI) === true) {
-    number = dni.slice(0, dni.length - 1)
-    number = number.replace('X', 0)
-    number = number.replace('Y', 1)
-    number = number.replace('Z', 2)
+    number = parseInt(
+      dni
+        .slice(0, dni.length - 1)
+        .replace('X', '0')
+        .replace('Y', '1')
+        .replace('Z', '2'),
+      10
+    )
     letter = DNI.slice(-1)
     number %= 23
     const letters = 'TRWAGMYFPDXBNJZSQVHLCKET'
     const correctLetter = letters.substring(number, number + 1)
+
     if (letter !== correctLetter) {
       return {
         error: true,
