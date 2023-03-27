fb1bcea15317e618ea5c7714379aa3551b0087e1
Halimeh <101599560+Lahali@users.noreply.github.com>
2023-03-27 11:01:08 +0200

    59-add icon component and its test

--- front/src/components/atoms/index.ts
+++ front/src/components/atoms/index.ts
@@ -4,5 +4,6 @@ import Button from './Button'
 import InputText from './InputText'
 import Text from './Text'
 import ValidationMessage from './ValidationMessage'
+import Icon from './Icon'
 
-export { InputText, Text, Label, Title, ValidationMessage, Button }
+export { InputText, Text, Label, Title, ValidationMessage, Button, Icon }
