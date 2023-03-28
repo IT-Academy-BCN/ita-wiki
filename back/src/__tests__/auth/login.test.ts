74acc787cb01a8efe5fd356571f8d802ac6ca9c3
Iván Legrán <68439095+ivanlegranbizarro@users.noreply.github.com>
2023-03-28 09:56:30 +0200

    improving test

--- back/src/__tests__/auth/login.test.ts
+++ back/src/__tests__/auth/login.test.ts
@@ -3,15 +3,18 @@ import supertest from 'supertest'
 import { expect, test } from 'vitest'
 import { server } from '../setup'
 
-test('/api/v1/auth/login', async () => {
+test('debería pasar con credenciales correctas. Chequea status 204 y si existe cookie llamada token', async () => {
   const response = await supertest(server).post('/api/v1/auth/login').send({
     dni: '45632452a',
     password: 'password'
   })
   expect(response.status).toBe(204)
+
+  const cookie = response.header['set-cookie'] as string[]
+  expect(cookie[0]).toMatch(/token/)
 })
 
-test('/api/v1/auth/login', async () => {
+test('debe fallar con credenciales incorrectas y devolver 400', async () => {
   const response = await supertest(server).post('/api/v1/auth/login').send({
     dni: '45632452a',
     password: 'wrong password'
