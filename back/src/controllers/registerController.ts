a5b2941b376cd6c689d8a795da48bbf2746d3ebb
Iván Legrán <68439095+ivanlegranbizarro@users.noreply.github.com>
2023-03-29 15:55:47 +0200

    fixing test
    script for test
    
    cleaning db
    
    clean datababase beforeAll

--- back/src/controllers/registerController.ts
+++ back/src/controllers/registerController.ts
@@ -8,41 +8,40 @@ export const registerController: Middleware = async (ctx: Context) => {
   try {
     const userByDni = await prisma.user.findUnique({
       where: { dni: dni.toUpperCase() as string },
-      select: { id: true },
+      select: { id: true }
     })
 
     if (userByDni) {
       ctx.status = 400
       ctx.body = {
-        error: 'DNI already exists',
+        error: 'DNI already exists'
       }
       return
     }
 
     const userByEmail = await prisma.user.findUnique({
       where: { email },
-      select: { id: true },
+      select: { id: true }
     })
 
     if (userByEmail) {
       ctx.status = 400
       ctx.body = {
-        error: 'Email already exists',
+        error: 'Email already exists'
       }
       return
     }
 
     const user = await prisma.user.create({
-      data: { dni: dni.toUpperCase(), password, name, email },
+      data: { dni: dni.toUpperCase(), password, name, email }
     })
 
     const token = jwt.sign({ userId: user.id }, process.env.JWT_KEY as Secret, {
-      expiresIn: '1d',
+      expiresIn: '1d'
     })
     ctx.cookies.set('token', token, { httpOnly: true })
     ctx.status = 204
-  }
-  catch (error) {
+  } catch (error) {
     ctx.status = 500
     ctx.body = { error }
   }
