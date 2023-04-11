0bbc540370624c2ebb66de312a9c8356da2eb032
Nazareno Valentini <nazareno.valentini@gmail.com>
2023-04-11 15:05:45 +0200

    80-fe-create-selectgrouptsx

---
+++ front/src/components/atoms/Select.tsx
@@ -0,0 +1,65 @@
+import { SelectHTMLAttributes, useState } from 'react'
+import styled from 'styled-components'
+import { colors, dimensions } from '../../styles'
+
+const SelectStyled = styled.select<TSelect>`
+  border-radius: ${dimensions.borderRadius.base};
+  border: 1px solid ${colors.gray.gray4};
+  color: ${colors.gray.gray3};
+  font-weight: 500;
+  padding: ${dimensions.spacing.sm};
+  width: 100%;
+
+  ${({ error }) => error && `border: 1px solid ${colors.error};`}
+
+  &:focus {
+    outline: 0 none;
+  }
+`
+
+type TOption = {
+  value: string
+  label: string
+}
+
+type TSelect = SelectHTMLAttributes<HTMLSelectElement> & {
+  options?: TOption[]
+  error?: boolean | string
+  placeholder?: string
+}
+
+function Select({
+  options = [],
+  error = false,
+  placeholder = 'Options',
+  ...rest
+}: TSelect) {
+  const [selectedOption, setSelectedOption] = useState<TOption | null>(null)
+
+  const handleSelectChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    const { value } = event.target
+    const option = options.find((op) => op.value === value)
+    setSelectedOption(option ?? null)
+  }
+  console.log('state SELECT', selectedOption)
+
+  return (
+    <SelectStyled
+      value={selectedOption?.value ?? ''}
+      onChange={handleSelectChange}
+      error={error}
+      {...rest}
+    >
+      <option disabled value="">
+        {placeholder}
+      </option>
+      {options.map(({ value, label }) => (
+        <option key={value} value={value}>
+          {label}
+        </option>
+      ))}
+    </SelectStyled>
+  )
+}
+
+export { Select, type TSelect }
