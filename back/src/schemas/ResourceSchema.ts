f69e3b252d44a261f3f2ab96d42b7914f5cabc70
Oriol Sastre <oriol.sastre@gmail.com>
2023-04-12 11:28:59 +0200

    resource + topic table/schema

---
+++ back/src/schemas/ResourceSchema.ts
@@ -0,0 +1,13 @@
+import { z } from '../openapi/zod'
+
+export const ResourceSchema = z.object({
+  id: z.string(),
+  title: z.string(),
+  description: z.string().optional(),
+  url: z.string().url(),
+  resource_type: z.enum(['BLOG', 'VIDEO', 'TUTORIAL']),
+  topicId: z.string(),
+  userId: z.string(),
+  createdAt: z.date(),
+  updatedAt: z.date()
+});
