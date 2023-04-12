150b34b17be317077841ce300868051a36ac06ab
Kevin <kevinmamaqi@users.noreply.github.com>
2023-04-12 11:45:19 +0200

    104 add schemas package (#105)
    * update
    
    * update

---
+++ packages/schemas/src/index.ts
@@ -0,0 +1,21 @@
+import { z } from "zod";
+
+const validNIEPrefixes = ["X", "Y", "Z"];
+const validDNIPrefixes = [...Array(23).keys()].map((i) => (i + 1).toString());
+const DNI_REGEX = /^(([XYZ]\d{7,8})|(\d{8}))([a-zA-Z])$/i;
+const DNISchema = z
+  .string()
+  .regex(DNI_REGEX)
+  .transform((value) => value.toUpperCase())
+  .refine((value) => {
+    const firstLetter = value.charAt(0);
+    return (
+      validDNIPrefixes.includes(firstLetter) ||
+      validNIEPrefixes.includes(firstLetter)
+    );
+  });
+
+const UserLoginSchema = z.object({
+  dni: DNISchema,
+  password: z.string().min(8),
+});
