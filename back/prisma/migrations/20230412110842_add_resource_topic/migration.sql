a40117e73fcb66c64675f8838992b75d4cc892fd
Oriol Sastre <oriol.sastre@gmail.com>
2023-04-12 14:07:09 +0200

    seed data

---
+++ back/prisma/migrations/20230412110842_add_resource_topic/migration.sql
@@ -0,0 +1,33 @@
+-- CreateEnum
+CREATE TYPE "RESOURCE_TYPE" AS ENUM ('BLOG', 'VIDEO', 'TUTORIAL');
+
+-- CreateTable
+CREATE TABLE "resources" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT,
+    "url" TEXT NOT NULL,
+    "resource_type" "RESOURCE_TYPE" NOT NULL,
+    "topicId" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "resources_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "topics" (
+    "id" TEXT NOT NULL,
+    "topic" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "topics_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "resources" ADD CONSTRAINT "resources_topicId_fkey" FOREIGN KEY ("topicId") REFERENCES "topics"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "resources" ADD CONSTRAINT "resources_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
