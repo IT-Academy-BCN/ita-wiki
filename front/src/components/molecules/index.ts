06459cb0b9934e0f2f4aacd378bb72d4dee91647
Iván Legrán <68439095+ivanlegranbizarro@users.noreply.github.com>
2023-04-16 10:58:32 +0200

    ok

--- front/src/components/molecules/index.ts
+++ front/src/components/molecules/index.ts
@@ -2,8 +2,6 @@ import InputGroup from './InputGroup'
 
 import SelectGroup from './SelectGroup'
 
-export { InputGroup, SelectGroup }
 import { Modal } from './Modal'
-import SelectGroup from './SelectGroup'
 
 export { InputGroup, Modal, SelectGroup }
