0fbe271525ba734b2cc83e4eaa62c457d7113645
Oriol Sastre <oriol.sastre@gmail.com>
2023-04-17 11:42:53 +0200

    tests completed

--- back/src/controllers/topicController.ts
+++ back/src/controllers/topicController.ts
@@ -2,7 +2,12 @@ import Koa, { Middleware } from 'koa'
 import { prisma } from '../prisma/client'
 
 export const getTopics: Middleware = async (ctx: Koa.Context) => {
-    const topics = await prisma.topic.findMany();
+    const topics = await prisma.topic.findMany({
+        select: {
+            id: true,
+            topic: true,
+        }
+    });
     ctx.status = 200;
-    ctx.body = { topics };
+    ctx.body = topics;
 }
--- back/src/routes/topicsRouter.ts
+++ back/src/routes/topicsRouter.ts
@@ -1,8 +1,5 @@
 import Router from '@koa/router'
-// import { z } from 'zod'
-// import { /* authMiddleware, */ validate } from '../middleware'
 import { getTopics } from '../controllers'
-// import { TopicSchema } from '../schemas'
 
 const topicsRouter = new Router()
 
@@ -10,7 +7,6 @@ topicsRouter.prefix('/api/v1/topics')
 
 topicsRouter.get(
   '/',
-  /* authMiddleware, */
   getTopics
 )
 
