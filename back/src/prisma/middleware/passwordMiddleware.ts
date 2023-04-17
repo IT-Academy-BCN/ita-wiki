96365baec1c45e2fda122de6e2b31e1ffd2c011d
Guillem Parrado <guillempado@gmail.com>
2023-04-17 17:53:54 +0200

    prisma passwordMiddleware CreateMany debug

--- back/src/prisma/middleware/passwordMiddleware.ts
+++ back/src/prisma/middleware/passwordMiddleware.ts
@@ -2,15 +2,19 @@ import { prisma } from '../client'
 import { hashPassword } from '../../helpers/passwordHash'
 
 prisma.$use(async (params, next) => {
-  if (params.model === 'User' && (
-      params.action === 'create' ||
-      params.action === 'update' ||
-      params.action === 'upsert' ||
-      params.action === 'createMany'
-    )) {
+  if (params.model === 'User'){
+    if(params.action === 'create' ||
+    params.action === 'update' ||
+    params.action === 'upsert'){
       // eslint-disable-next-line no-param-reassign
       params.args.data.password = await hashPassword(params.args.data.password)
     }
-  
+    else if(params.action === 'createMany'){
+      // eslint-disable-next-line no-param-reassign, no-restricted-syntax
+      for (const user of params.args.data) {
+        user.password = await hashPassword(user.password)
+      }
+    }
+  }   
   return next(params)
 })
