1caabe5309e6fa8363bb9c564ed95ef64eb963b4
Halimeh <101599560+Lahali@users.noreply.github.com>
2023-04-19 22:40:24 +0200

    133-added active links

--- front/src/pages/Home.tsx
+++ front/src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import { FC } from 'react'
+import { FC, SetStateAction, useState } from 'react'
 import styled from 'styled-components'
 import { FlexBox, colors, dimensions } from '../styles'
 import { CardResource, InputGroup } from '../components/molecules'
@@ -23,17 +23,13 @@ const DivStyled = styled.div`
   width: 100%;
   height: 100%;
   border-radius: ${dimensions.borderRadius.base};
-  overflow: scroll;
-
-  &::-webkit-scrollbar {
-    display: none;
-  }
 `
 
 const CategoriesContainerStyled = styled(FlexBox)`
   padding: ${dimensions.spacing.lg};
   margin-right: 1rem;
   align-items: flex-start;
+  color: ${colors.gray.gray3};
 `
 
 const UserResourcesContainerStyled = styled(FlexBox)`
@@ -52,6 +48,11 @@ const SideColumnContainer = styled(FlexBox)`
   align-items: flex-start;
   flex-grow: 1;
   padding: 2rem;
+  overflow: scroll;
+
+  &::-webkit-scrollbar {
+    display: none;
+  }
 `
 
 const MiddleColumnContainer = styled(FlexBox)`
@@ -61,6 +62,11 @@ const MiddleColumnContainer = styled(FlexBox)`
   border-left: solid 1px black;
   justify-content: flex-start;
   align-items: flex-start;
+  overflow: scroll;
+
+  &::-webkit-scrollbar {
+    display: none;
+  }
 `
 
 const VideoContainer = styled(FlexBox)`
@@ -75,11 +81,31 @@ const VideoContainer = styled(FlexBox)`
 const ImgStyled = styled.img`
   height: 30px;
   margin-right: ${dimensions.spacing.base};
+  margin-top: ${dimensions.spacing.xxl};
 `
 
 const TextStyled = styled(Text)`
   margin: 0rem;
   margin-top: 5px;
+`
+
+type TLinkStyled = {
+  active?: boolean
+}
+
+const LinkStyled = styled.a<TLinkStyled>`
+  color: ${({ active }) => (active ? colors.primary : colors.gray.gray3)};
+  font-weight: ${({ active }) => (active ? 'bold' : 'regular')};
+  border: ${({ active }) => (active ? 'solid 1px black' : 'none')};
+  border-radius: ${dimensions.borderRadius.base};
+  padding: ${dimensions.spacing.xxs};
+  width: 100%;
+  margin-top: ${dimensions.spacing.base};
+`
+const CategoryLinkStyled = styled.a<TLinkStyled>`
+  color: ${({ active }) => (active ? colors.black.black3 : colors.gray.gray3)};
+  font-weight: ${({ active }) => (active ? 'bold' : 'regular')};
+  margin-top: ${dimensions.spacing.xxl};
 `
 
 const dataSubjects = [
@@ -130,120 +156,147 @@ const resources = [
   },
 ]
 
-const Home: FC = () => (
-  <MainContainer>
-    <CategoriesContainerStyled>
-      <FlexBox direction="row">
-        <ImgStyled src={icons.javascript} alt="javascript logo" />
-        <p>Javascript</p>
-      </FlexBox>
-      <FlexBox direction="row">
-        <ImgStyled src={icons.react} alt="react logo" />
-        <p>React</p>
-      </FlexBox>
-      <FlexBox direction="row">
-        <ImgStyled src={icons.angular} alt="angular logo" />
-        <p>Angular</p>
-      </FlexBox>
-      <FlexBox direction="row">
-        <ImgStyled src={icons.dataScience} alt="datascience logo" />
-        <p>Data Science</p>
-      </FlexBox>
-    </CategoriesContainerStyled>
-    {/* ==> CONTAINER CON LAS LAS COLUMNAS */}
-    <DivStyled>
-      {/* ==> COLUMNA BÚSQUEDA */}
-      <SideColumnContainer>
-        <InputGroup
-          label="search-resource"
-          name="search-resource"
-          placeholder="Buscar recurso concreto"
-          id="search-resource"
-        />
-        <Text fontWeight="bold">Temas de React</Text>
-        {dataSubjects.map((sub) => (
-          <Text key={sub.id} color={colors.gray.gray3}>
-            {sub.subject}
-          </Text>
-        ))}
-      </SideColumnContainer>
-      {/* ==> COLUMNA RECURSOS */}
-      <MiddleColumnContainer>
-        <Title as="h2" fontWeight="bold">
-          Recursos Componentes
-        </Title>
-        {/* ==> LÍNEA DE VÍDEOS, VOTOS Y FECHA */}
-        <FlexBox
-          justify="space-between"
-          direction="row"
-          style={{ width: '100%' }}
-        >
-          {/* ==> VÍDEOS */}
-          <VideoContainer>
-            <Text fontWeight="bold">Vídeos</Text>
-            <Icon name="expand_more" />
-          </VideoContainer>
-          {/* ==> VOTOS Y FECHA */}
-          <FlexBox direction="row">
-            <FlexBox direction="row">
-              <Text fontWeight="bold">Votos</Text>
-              <Icon name="arrow_downward" />
-            </FlexBox>
-            <Text color={colors.gray.gray3}>Fecha</Text>
-          </FlexBox>
-        </FlexBox>
-        {resources.map((resource) => (
-          <CardResource
-            createdBy={resource.name}
-            createdOn={resource.date}
-            title={resource.title}
-            likes={10}
-            description={resource.subtitle}
-          />
-        ))}
-      </MiddleColumnContainer>
-      {/* ==> COLUMNA USUARIO */}
-      <SideColumnContainer>
-        {/* TÍTULO 1 */}
-        <ContainerGapStyled>
-          <Icon name="favorite" fill={0} />
-          <Title as="h2" fontWeight="bold">
-            Recursos favoritos
-          </Title>
-        </ContainerGapStyled>
-        {/* ==> CONTENIDO FAVORITOS */}
-        {resources.map((fav) => (
-          <UserResourcesContainerStyled>
-            <TextStyled key={fav.id} fontWeight="bold">
-              {fav.title}
-            </TextStyled>
-            <TextStyled fontSize="small" color={colors.gray.gray3}>
-              {fav.subtitle}
-            </TextStyled>
-          </UserResourcesContainerStyled>
-        ))}
+const categories = [
+  {
+    id: 1,
+    category: 'Javascript',
+    image: icons.javascript,
+    alt: 'javascript logo',
+  },
+  { id: 2, category: 'React', image: icons.react, alt: 'react logo' },
+  { id: 3, category: 'Angular', image: icons.angular, alt: 'angular logo' },
+  {
+    id: 4,
+    category: 'Data Science',
+    image: icons.dataScience,
+    alt: 'data science logo',
+  },
+]
 
-        {/* TÍTULO 2 */}
-        <ContainerGapStyled>
-          <Icon name="menu_book" fill={0} />
-          <Title as="h2" fontWeight="bold">
-            Mis recursos
-          </Title>
-        </ContainerGapStyled>
-        {/* ==> CONTENIDO MIS RECURSOS */}
-        {resources.map((res) => (
-          <UserResourcesContainerStyled>
-            <TextStyled key={res.id} fontWeight="bold">
-              {res.title}
-            </TextStyled>
-            <TextStyled fontSize="small" color={colors.gray.gray3}>
-              {res.subtitle}
-            </TextStyled>
-          </UserResourcesContainerStyled>
+const Home: FC = () => {
+  const [activeLink, setActiveLink] = useState('')
+  const [activeCategory, setActiveCategory] = useState('')
+  const handleClick = (link: SetStateAction<string>) => {
+    setActiveLink(link)
+  }
+  const handleCategoryClick = (cat: SetStateAction<string>) => {
+    setActiveCategory(cat)
+  }
+
+  return (
+    <MainContainer>
+      <CategoriesContainerStyled>
+        {categories.map((category) => (
+          <FlexBox direction="row" key={category.id}>
+            <ImgStyled src={category.image} alt={category.alt} />
+            <CategoryLinkStyled
+              active={activeCategory === category.category}
+              onClick={() => handleCategoryClick(category.category)}
+            >
+              {category.category}
+            </CategoryLinkStyled>
+          </FlexBox>
         ))}
-      </SideColumnContainer>
-    </DivStyled>
-  </MainContainer>
-)
+      </CategoriesContainerStyled>
+      {/* ==> CONTAINER CON LAS LAS COLUMNAS */}
+      <DivStyled>
+        {/* ==> COLUMNA BÚSQUEDA */}
+        <SideColumnContainer>
+          <InputGroup
+            label="search-resource"
+            name="search-resource"
+            placeholder="Buscar recurso concreto"
+            id="search-resource"
+          />
+          <Text fontWeight="bold">Temas de React</Text>
+          {dataSubjects.map((sub) => (
+            <LinkStyled
+              key={sub.id}
+              active={activeLink === sub.subject}
+              onClick={() => handleClick(sub.subject)}
+            >
+              {sub.subject}
+            </LinkStyled>
+          ))}
+        </SideColumnContainer>
+        {/* ==> COLUMNA RECURSOS */}
+        <MiddleColumnContainer>
+          <Title as="h2" fontWeight="bold">
+            Recursos Componentes
+          </Title>
+          {/* ==> LÍNEA DE VÍDEOS, VOTOS Y FECHA */}
+          <FlexBox
+            justify="space-between"
+            direction="row"
+            style={{ width: '100%' }}
+          >
+            {/* ==> VÍDEOS */}
+            <VideoContainer>
+              <Text fontWeight="bold">Vídeos</Text>
+              <Icon name="expand_more" />
+            </VideoContainer>
+            {/* ==> VOTOS Y FECHA */}
+            <FlexBox direction="row">
+              <FlexBox direction="row">
+                <Text fontWeight="bold">Votos</Text>
+                <Icon name="arrow_downward" />
+              </FlexBox>
+              <Text color={colors.gray.gray3}>Fecha</Text>
+            </FlexBox>
+          </FlexBox>
+          {resources.map((resource) => (
+            <CardResource
+              createdBy={resource.name}
+              createdOn={resource.date}
+              title={resource.title}
+              likes={10}
+              description={resource.subtitle}
+            />
+          ))}
+        </MiddleColumnContainer>
+        {/* ==> COLUMNA USUARIO */}
+        <SideColumnContainer>
+          {/* TÍTULO 1 */}
+          <ContainerGapStyled>
+            <Icon name="favorite" fill={0} />
+            <Title as="h2" fontWeight="bold">
+              Recursos favoritos
+            </Title>
+          </ContainerGapStyled>
+          {/* ==> CONTENIDO FAVORITOS */}
+          {resources.map((fav) => (
+            <UserResourcesContainerStyled>
+              <TextStyled key={fav.id} fontWeight="bold">
+                {fav.title}
+              </TextStyled>
+              <TextStyled fontSize="small" color={colors.gray.gray3}>
+                {fav.subtitle}
+              </TextStyled>
+            </UserResourcesContainerStyled>
+          ))}
+
+          {/* TÍTULO 2 */}
+          <ContainerGapStyled>
+            <Icon name="menu_book" fill={0} />
+            <Title as="h2" fontWeight="bold">
+              Mis recursos
+            </Title>
+          </ContainerGapStyled>
+          {/* ==> CONTENIDO MIS RECURSOS */}
+          {resources.map((res) => (
+            <UserResourcesContainerStyled>
+              <TextStyled key={res.id} fontWeight="bold">
+                {res.title}
+              </TextStyled>
+              <TextStyled fontSize="small" color={colors.gray.gray3}>
+                {res.subtitle}
+              </TextStyled>
+            </UserResourcesContainerStyled>
+          ))}
+        </SideColumnContainer>
+      </DivStyled>
+    </MainContainer>
+  )
+}
 
 export default Home
