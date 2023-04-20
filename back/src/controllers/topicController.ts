bc7cde23bd970c0208cac6568ccc77a35c35331f
danimorera <danimoreramunoz@gmail.com>
2023-04-20 23:09:15 +0200

    getTopicsByCategoryId controller created

--- back/src/controllers/topicController.ts
+++ back/src/controllers/topicController.ts
@@ -11,3 +11,18 @@ export const getTopics: Middleware = async (ctx: Koa.Context) => {
     ctx.status = 200;
     ctx.body = topics;
 }
+
+export const getTopicsByCategoryId: Middleware = async (ctx: Koa.Context) => {
+    const {categoryId} = ctx.request.body;
+    const topics = await prisma.topic.findMany({
+        where: {
+            categoryId
+        },
+        select: {
+            id: true,
+            name: true,
+        }
+    });
+    ctx.status = 200;
+    ctx.body = topics;
+}
