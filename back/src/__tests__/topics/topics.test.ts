d5f009d13bb984a2ac53ccfa8d9a0eb19cb58d76
danimorera <danimoreramunoz@gmail.com>
2023-04-21 01:19:10 +0200

    topic.test modified to include new controller

--- back/src/__tests__/topics/topics.test.ts
+++ back/src/__tests__/topics/topics.test.ts
@@ -6,7 +6,7 @@ describe('Testing topics endpoint', () => {
     describe('Testing GET method',  () => {
         test('Should respond OK status and return topics as an array. As per seed data, it should not be empty, and contain objects with an id and topic.', async () => {
             const response = await supertest(server).get('/api/v1/topics')
-            
+
             expect(response.status).toBe(200);
             expect(response.body).toBeInstanceOf(Array)
             expect(response.body.length).toBeGreaterThan(0)
@@ -18,6 +18,25 @@ describe('Testing topics endpoint', () => {
                     })
                 ])
             )
+        })
+    })
+
+    describe('Testing GET /topics/:categoryId', () => {
+        test('Should respond OK status and return topics as an array. As per seed data, it should not be empty, and contain objects with an id and topic.', async () => {
+            const response = await supertest(server).get('/api/v1/topics/clgpl2wko0002f94gedbkos1g')
+
+            expect(response.status).toBe(200);
+            expect(response.body).toBeInstanceOf(Array)
+            expect(response.body).toEqual([
+                {
+                    id: 'clgpl2wl50007f94glqhh1fut',
+                    name: 'Components'
+                },
+                {
+                    id: 'clgpl2wl50009f94gsu5d9sp6',
+                    name: 'Listas'
+                }
+            ]);
         })
     })
 })
