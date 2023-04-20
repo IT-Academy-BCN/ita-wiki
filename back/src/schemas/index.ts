bc4c92655abaafa79b2a45467ec5b5af6d9c81ee
Guillem Parrado <guillempado@gmail.com>
2023-04-20 12:53:55 +0200

    minor

--- back/src/schemas/index.ts
+++ back/src/schemas/index.ts
@@ -1,6 +1,6 @@
 export { userSchema } from './userSchema'
 export { userLoginSchema } from './userLoginSchema'
 export { userRegisterSchema } from './userRegisterSchema'
-export { resourceSchema as ResourceSchema } from './resourceSchema'
+export { resourceSchema } from './resourceSchema'
 export { resourceCreateSchema } from './resourceCreateSchema'
 export { topicSchema } from './topicSchema'
