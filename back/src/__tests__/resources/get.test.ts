ddbfc8285b579b1c48fde6f519e7ae63d40380e8
Guillem Parrado <guillempado@gmail.com>
2023-04-23 19:28:37 +0200

    getResources, tests

--- back/src/__tests__/resources/get.test.ts
+++ back/src/__tests__/resources/get.test.ts
@@ -14,25 +14,92 @@ describe('Testing resources get endpoint', () => {
     authToken = response.header['set-cookie'][0].split(';')[0]
   })
 
-  test('should get all resources by type ', async () => {
+  test('should get all resources by resourceType ', async () => {
     const response = await supertest(server)
       .get('/api/v1/resources')
       .set('Cookie', authToken)
       .query({ type: 'BLOG' })
       
       expect(response.status).toBe(200)
-      expect(response.body.resources.length).toBe(2)
-      expect(response.body.resources[0].type).toBe('BLOG')
+      expect(response.body.resources.length).toBeGreaterThanOrEqual(2)
+      response.body.resources.map((resource : any) => expect(resource.resourceType).toBe('BLOG'))
   })
 
-  // test('should get all resources by category ', async () => {})
+  test('should fail with wrong resourceType', async () => {
+    const response = await supertest(server)
+      .get('/api/v1/resources')
+      .set('Cookie', authToken)
+      .query({ type: 'WRONG TYPE' })
+      
+      expect(response.status).toBe(500)
+  })
 
-  // test('should get all resources by topic ', async () => {})
+  test('should get all resources by topic ', async () => {
 
-  // test('should get all resources by type and category ', async () => {})
+    const topicName = 'Listas'
 
-  // test('should get all resources by type and topic ', async () => {})
+    const response = await supertest(server)
+      .get('/api/v1/resources')
+      .set('Cookie', authToken)
+      .query({ topic: topicName })
+      
+      expect(response.status).toBe(200)
+      expect(response.body.resources.length).toBeGreaterThanOrEqual(2)
 
-  // test('should fail with category and topic ', async () => {})
+      response.body.resources.forEach((resource : any) => {
+        expect(
+          resource.topics.map((t : any) => t.name)
+        ).toContain(topicName)
+      })
+  })
 
+  test('should fail without a valid topic', async () => {
+
+    const topicName = 'This topic does not exist'
+
+    const response = await supertest(server)
+      .get('/api/v1/resources')
+      .set('Cookie', authToken)
+      .query({ topic: topicName })
+      
+      expect(response.status).toBe(200)
+      expect(response.body.resources.length).toBe(0)
+  })
+
+  test('should get all resources by type and topic ', async () => {
+    const topicName = 'Listas'
+
+    const response = await supertest(server)
+      .get('/api/v1/resources')
+      .set('Cookie', authToken)
+      .query({ topic: topicName })
+      
+      expect(response.status).toBe(200)
+      expect(response.body.resources.length).toBeGreaterThanOrEqual(2)
+
+      response.body.resources.forEach((resource : any) => {
+        expect(
+          resource.topics.map((t : any) => t.name)
+        ).toContain(topicName)
+        expect(resource.resourceType).toBe('BLOG')
+      })
+
+  })
+
+  test('should get all resources', async () => {
+
+    const response = await supertest(server)
+      .get('/api/v1/resources')
+      .set('Cookie', authToken)
+      .query({})
+      
+      expect(response.status).toBe(200)
+
+      const titles = response.body.resources.map((resource : any) => resource.title)
+
+      expect(titles).toContain("My resource in React")
+      expect(titles).toContain("My resource in Node")
+      expect(titles).toContain("My second resource in React")
+      expect(titles).toContain("My resource in Javascript")
+  })
 })
--- back/src/controllers/getResourcesController.ts
+++ back/src/controllers/getResourcesController.ts
@@ -2,38 +2,45 @@ import Koa, { Middleware } from 'koa'
 import { prisma } from '../prisma/client'
 
 export const getResources: Middleware = async (ctx: Koa.Context) => {
-    const { type, category, topic } = ctx.query
-    
-    if(category && topic){
-      ctx.status = 422
-      ctx.body = {
-        error: 'Category and topic are mutually exclusive'
-      }
-      return
-    }
+  const { type, topic } = ctx.query
 
-    const where = {}
-    if (type)
-      // @ts-ignore
-      where.type = { equals: type }
-    if (category)
-      // @ts-ignore
-      where.category = { equals: category }
-    if (topic)
-      // @ts-ignore
-      where.topic = { equals: topic }
-  
-    const resources = await prisma.resource
-      .findMany({
-        where,
-        include: {
-          topics: true,
-          user: true,
-        },
-      })
-      
-  
-    ctx.status = 200
-    ctx.body = { resources }
-  }
-  
+  const where = {}
+  if (type)
+    // @ts-ignore
+    where.resourceType = { equals: type }
+  if (topic)
+    // @ts-ignore
+    where.topics = { some: { topic: { name: topic } } }
+
+  const resourcesDAO = await prisma.resource.findMany({
+    where,
+    include: {
+      topics: true,
+      user: true,
+    },
+  })
+
+  // `include topics: true` only maps to relational table info
+  // we also need to produce data for consumption
+  const resources = await Promise.all(
+    resourcesDAO.map(async (r) => ({
+      id: r.id,
+      title: r.title,
+      description: r.description,
+      url: r.url,
+      resourceType: r.resourceType,
+      createdAt: r.createdAt,
+      updatedAt: r.updatedAt,
+      userEmail: r.user.email,
+      topics: await Promise.all(r.topics.map(
+        async (t) =>
+          await prisma.topic.findUnique({
+            where: { id: t.topicId }
+          })
+      )),
+    }))
+  )
+
+  ctx.status = 200
+  ctx.body = { resources }
+}
