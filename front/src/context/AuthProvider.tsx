83769d834c1ed6a090098c9ec6503252b1c34e5e
Halimeh <101599560+Lahali@users.noreply.github.com>
2023-04-25 18:15:15 +0200

    180-created fetch

--- front/src/context/AuthProvider.tsx
+++ front/src/context/AuthProvider.tsx
@@ -1,13 +1,21 @@
-import React, { createContext, useContext, useMemo, useState } from 'react'
+import React, {
+  createContext,
+  useContext,
+  useEffect,
+  useMemo,
+  useState,
+} from 'react'
 
 type TAuthContext = {
   isLoggedIn: boolean
+  user: object
   children: React.ReactNode
 }
 
 export const authContext = createContext<TAuthContext>({
   isLoggedIn: false,
   children: null,
+  user: {},
 })
 
 // hook
@@ -18,10 +26,33 @@ export const useAuth = () => {
 
 export const AuthProvider: React.FC<TAuthContext> = ({ children }) => {
   const [isLoggedIn, setIsLoggedIn] = useState(false)
+  const [user, setUser] = useState({})
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState(null)
+
+  const url =
+    'https://dev.api.itadirectory.eurecatacademy.org/api/v1/auth/login'
+
   const value = useMemo(
-    () => ({ isLoggedIn, children }),
-    [children, isLoggedIn]
+    () => ({ isLoggedIn, children, user, loading, error }),
+    [children, error, isLoggedIn, loading, user]
   )
+
+  useEffect(() => {
+    setLoading(true)
+    setError(null)
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        setUser(data)
+        setIsLoggedIn(true)
+        setLoading(false)
+      })
+      .catch((err) => {
+        setError(err)
+        setLoading(false)
+      })
+  }, [url])
 
   return <authContext.Provider value={value}>{children}</authContext.Provider>
 }
