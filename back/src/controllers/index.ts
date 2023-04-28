e628f997bd75a97330ba948ad62554d0fc436af1
Oriol Sastre Rienitz <51907013+oriolsastre@users.noreply.github.com>
2023-04-28 09:55:12 +0200

    149 be getresourcebyuserid (#205)
    * resourceByUserId controller
    
    * minor index controller
    
    * swagger + test
    
    * update
    
    ---------
    
    Co-authored-by: Kevin Mamaqi <kevinmamaqi@gmail.com>

--- back/src/controllers/index.ts
+++ back/src/controllers/index.ts
@@ -1,6 +1,6 @@
 export { loginController } from './auth/loginController'
 export { registerController } from './auth/registerController'
 export { authMeController } from './auth/authMeController'
-export { createResource } from './resourceController'
+export { createResource, getResourcesByUserId } from './resourceController'
 export { getTopics, getTopicsByCategoryId } from './topicController'
 export { getCategories } from './categoryController'
