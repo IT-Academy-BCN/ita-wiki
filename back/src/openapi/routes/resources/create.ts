bdbc379bc411ba827425e7fed357dd786d2600af
Oriol Sastre <oriol.sastre@gmail.com>
2023-05-02 14:52:33 +0200

    resource post docs, missing responses

---
+++ back/src/openapi/routes/resources/create.ts
@@ -0,0 +1,23 @@
+import { pathRoot } from "../../../routes/routes";
+import { resourceCreateSchema } from "../../../schemas";
+import { cookieAuth } from "../../components/cookieAuth";
+import { registry } from "../../registry";
+
+registry.registerPath({
+    method: 'post',
+    tags: ['resources'],
+    path: `${pathRoot.v1.resources}/create`,
+    description: 'Allows a logged in user to post a resource.',
+    summary: 'Post a resource',
+    security: [{ [cookieAuth.name]: [] }],
+    request: {
+        body: {
+            content: {
+                'application/json': {
+                    schema: resourceCreateSchema,
+                },
+            },
+        },
+    },
+    responses: {}
+})
