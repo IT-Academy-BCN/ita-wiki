6f88804d99ce1b21482aecba7b45fc6b281acfe6
danimorera <danimoreramunoz@gmail.com>
2023-05-09 09:27:41 +0200

    category slug can be null

--- back/prisma/migrations/20230509072440_category_slug_null/migration.sql
+++ back/prisma/migrations/20230509072440_category_slug_null/migration.sql
@@ -2,11 +2,10 @@
   Warnings:
 
   - A unique constraint covering the columns `[slug]` on the table `category` will be added. If there are existing duplicate values, this will fail.
-  - Added the required column `slug` to the `category` table without a default value. This is not possible if the table is not empty.
 
 */
 -- AlterTable
-ALTER TABLE "category" ADD COLUMN     "slug" TEXT NOT NULL;
+ALTER TABLE "category" ADD COLUMN     "slug" TEXT;
 
 -- CreateIndex
 CREATE UNIQUE INDEX "category_slug_key" ON "category"("slug");
