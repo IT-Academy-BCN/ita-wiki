5dac4b9e24b1c2e34dcff23b8d29d619fde9e227
llopFilms <llopfilms@gmail.com>
2023-05-11 20:07:26 +0200

    Simple test HomePage 'renders correctly'

--- front/src/__tests__/pages/HomePage.test.tsx
+++ front/src/__tests__/pages/HomePage.test.tsx
@@ -1,13 +1,17 @@
 import { render } from '@testing-library/react'
 import { BrowserRouter } from 'react-router-dom'
 import { Home } from '../../pages'
+import { queryClient } from '../setup'
+import { QueryClientProvider } from '@tanstack/react-query'
 
 describe('Test HomePage', () => {
   it('renders correctly', () => {
     render(
-      <BrowserRouter>
-        <Home />
-      </BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <Home />
+        </BrowserRouter>
+      </QueryClientProvider>
     )
   })
 })
