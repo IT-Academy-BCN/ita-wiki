f1205227ceade2976b3c7a237852f8df99eec05e
Natàlia <nataliaroca30@gmail.com>
2023-05-14 18:22:34 +0200

    Resource changed

--- front/src/pages/Resource.tsx
+++ front/src/pages/Resource.tsx
@@ -213,8 +213,8 @@ const Resource: FC = () => {
         <CardResource
           img={sd?.img}
           key={sd.createdOn}
-          likes={sd.likes}
           title={sd.title}
+          url={sd.url}
           description={sd.description}
           createdBy={sd.createdBy}
           createdOn={sd.createdOn}
