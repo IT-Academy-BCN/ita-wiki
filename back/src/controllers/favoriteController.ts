420f27c741031007a3d677c4bf80fab67ac3b388
danimorera <danimoreramunoz@gmail.com>
2023-05-15 23:49:33 +0200

    added existing user validation to controller

--- back/src/controllers/favoriteController.ts
+++ back/src/controllers/favoriteController.ts
@@ -2,14 +2,21 @@ import Koa, { Middleware } from 'koa'
 import { prisma } from '../prisma/client'
 
 export const getUserFavoriteResources: Middleware = async (ctx: Koa.Context) => {
-    const { userId } = ctx.params;
 
+    const { userId } = ctx.params;
     if (!userId) {
         ctx.status = 400;
         ctx.body = { error: 'User ID is required' };
         return;
-      }
-      
+    }
+
+    const user = await prisma.user.findUnique({ where: { id: userId } });
+    if (!user) {
+        ctx.status = 404;
+        ctx.body = { error: 'User not found' };
+        return;
+    }
+
     const favorites = await prisma.favorites.findMany({
         where: {
             userId
