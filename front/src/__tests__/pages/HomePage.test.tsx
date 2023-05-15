aad7287febe03e2d22eed69d5b494fb94ba53415
Kevin Mamaqi <kevinmamaqi@gmail.com>
2023-05-15 10:45:35 +0200

    update

--- front/src/__tests__/pages/HomePage.test.tsx
+++ front/src/__tests__/pages/HomePage.test.tsx
@@ -1,94 +1,24 @@
-import styled from 'styled-components'
-import { render } from '@testing-library/react'
-import { QueryClientProvider } from '@tanstack/react-query'
-import { BrowserRouter } from 'react-router-dom'
-import { queryClient } from '../setup'
 import { Home } from '../../pages'
-import { device } from '../../styles'
 import { resources } from '../../pages/Home'
+import { render } from '../test-utils'
 
-describe('Test HomePage', () => {
+describe('HomePage', () => {
   it('renders correctly', () => {
-    render(
-      <QueryClientProvider client={queryClient}>
-        <BrowserRouter>
-          <Home />
-        </BrowserRouter>
-      </QueryClientProvider>
+    render(<Home />)
+  })
+
+  it('filters items when the user search', () => {
+    // Encontrar el buscador
+    // Escribir en el buscador
+    // Comprobar que inicialmente estan todos los elementos
+    // Comprobar que cuando el usuario escribe solo estan los elementos que coinciden con su busqueda
+    const { getByText } = render(<Home />)
+    const filteredItems = resources.filter((resource) =>
+      resource.title.toLowerCase().includes('javascript')
+    )
+    expect(filteredItems).toBeInTheDocument()
+    filteredItems.forEach((resource) =>
+      expect(getByText(resource.title)).toBeInTheDocument()
     )
   })
-
-  describe('MobileStyled component Media Query behaviour', () => {
-    const MobileStyled = styled.div`
-      display: block;
-      @media only ${device.Laptop} {
-        display: none;
-      }
-    `
-
-    it('displays on Tablet and Mobile', () => {
-      const { getByTestId } = render(
-        <MobileStyled data-testid="mobile-styled" />
-      )
-      expect(getByTestId('mobile-styled')).toHaveStyle(`
-      display: block;
-    `)
-    })
-
-    it('hides on Laptop, Desktop and LaptopLg', () => {
-      const { getByTestId } = render(
-        <MobileStyled data-testid="mobile-styled" />
-      )
-      expect(getByTestId('mobile-styled')).toHaveStyle(`
-      display: none;
-    `)
-    })
-  })
-
-  describe('DesktopStyled component Media Query behaviour', () => {
-    const DesktopStyled = styled.div`
-      display: none;
-      @media only ${device.Laptop} {
-        display: block;
-      }
-    `
-
-    it('hides on Tablet and Mobile', () => {
-      const { getByTestId } = render(
-        <DesktopStyled data-testid="desktop-styled" />
-      )
-      expect(getByTestId('desktop-styled')).toHaveStyle(`
-      display: none;
-    `)
-    })
-
-    it('displays on Laptop, Desktop and LaptopLg', () => {
-      const { getByTestId } = render(
-        <DesktopStyled data-testid="desktop-styled" />
-      )
-      expect(getByTestId('desktop-styled')).toHaveStyle(`
-      display: block;
-    `)
-    })
-  })
-
-  describe('Test HomePage', () => {
-    it('renders filteredItems', () => {
-      const { getByText } = render(
-        <QueryClientProvider client={queryClient}>
-          <BrowserRouter>
-            <Home />
-          </BrowserRouter>
-        </QueryClientProvider>
-      )
-      const filteredItems = resources.filter((resource) =>
-        resource.title.toLowerCase().includes('javascript')
-      )
-      expect(filteredItems).toBeInTheDocument()
-      filteredItems.forEach((resource) =>
-        expect(getByText(resource.title)).toBeInTheDocument()
-      )
-    })
-  })
-
 })
