6e6ecea9f5446328b6cd5e4c8326667cd39c7da5
Kevin Mamaqi <kevinmamaqi@gmail.com>
2023-05-15 10:39:33 +0200

    update

--- front/src/pages/Home.tsx
+++ front/src/pages/Home.tsx
@@ -14,7 +14,7 @@ import {
   CategoriesList,
   ResourcesList,
 } from '../components/organisms'
-import { useSearch } from '../utils/hooks/useSearch'
+import { useSearch } from '../hooks'
 
 /* type TCategories = {
   id: number
@@ -33,44 +33,6 @@ export type TResource = {
   img: string
   url: string
 }
-
-/* const categories: TCategories[] = [
-  {
-    id: 1,
-    img: icons.angular,
-    resources: 49,
-    category: 'Angular',
-    topics: 6,
-  },
-  {
-    id: 2,
-    img: icons.react,
-    category: 'React',
-    resources: 65,
-    topics: 7,
-  },
-  {
-    id: 3,
-    img: icons.vue,
-    category: 'Vue',
-    resources: 32,
-    topics: 8,
-  },
-  {
-    id: 4,
-    img: icons.javascript,
-    category: 'Javascript',
-    resources: 44,
-    topics: 3,
-  },
-  {
-    id: 5,
-    img: icons.dataScience,
-    category: 'Data Science',
-    resources: 23,
-    topics: 1,
-  },
-] */
 
 export const resources: TResource[] = [
   {
