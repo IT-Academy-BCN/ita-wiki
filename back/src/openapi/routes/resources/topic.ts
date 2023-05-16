818b11c6e09c4bb9bfe01f46fc7a71c348e62c0f
reinevernunft13 <96286318+reinevernunft13@users.noreply.github.com>
2023-05-16 08:56:26 +0200

    added swagger

---
+++ back/src/openapi/routes/resources/topic.ts
@@ -0,0 +1,65 @@
+import { registry } from '../../registry'
+import { z } from '../../zod'
+import { pathRoot } from '../../../routes/routes'
+
+registry.registerPath({
+    method: 'get',
+    tags: ['resources'],
+    path: `${pathRoot.v1.resources}/topic/:topicId`,
+    summary: 'Returns a list of resources by topic ID',
+    description: 'Takes in a valid topic ID and returns a list of resources by topic',
+    request: {
+        params: z.object({
+            topicId: z.string().trim().min(1).openapi({
+                description: "ID of topic for which to retrieve resources",
+            })
+        })
+    },
+    responses: {
+        200: {
+            description: 'Resources found',
+
+        },
+        404: {
+
+            description: 'Topic not found',
+        }
+    
+    },
+});
+
+registry.registerPath({
+    method: 'get',
+    tags: ['resources'],
+    path: `${pathRoot.v1.resources}/topic/slug/:slug`,
+    summary: 'Returns a list of resources by topic slug',
+    description: 'Takes in a valid topic slug and returns a list of resources associated with that topic slug',
+    request: {
+        params: z.object({
+            slug: z.string().trim().min(1).openapi({
+                description: "topic slug associated with the resources",
+            }),
+        })
+    },
+    responses: {
+        200: {
+            description: 'Resources found',
+            content: {
+                'application/json': {
+                    schema: z.array(
+                        z.object({
+                            id: z.string().trim().min(1),
+                            slug: z.string().trim().min(1).openapi({ example: 'eventos'})
+                        })
+                    )
+            }
+
+        },
+    },
+        404: {
+
+            description: 'Topic not found',
+        }
+    
+    },
+});
