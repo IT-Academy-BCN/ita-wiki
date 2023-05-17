012b2931321b053455c38c243c06dbb848e51b26
Vincenzo- <viulenzgg@gmail.com>
2023-05-17 12:44:11 +0200

    Update urls.ts

--- front/src/constants/urls.ts
+++ front/src/constants/urls.ts
@@ -1,11 +1,6 @@
 export const urls: Record<string, string> = {
-  // auth
   register: '/api/v1/auth/register',
   getMe: '/api/v1/auth/me',
-
-  // categories
   getCategories: '/api/v1/categories',
-
-  // resources
   vote: '/api/v1/resources/vote/:resourceId/:vote',
 }
