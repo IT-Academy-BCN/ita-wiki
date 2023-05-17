a0785fe60ce95ca3ce6554df7e2f473d21c0d3ba
danimorera <danimoreramunoz@gmail.com>
2023-05-17 09:12:43 +0200

    created resources data for test, stil bugged

--- back/src/__tests__/favorites/favorites.test.ts
+++ back/src/__tests__/favorites/favorites.test.ts
@@ -1,31 +1,80 @@
 import supertest from 'supertest'
-import { expect, test, describe } from 'vitest'
-import { server } from '../globalSetup'
+import { expect, test, describe, beforeAll, afterAll } from 'vitest'
+import { server, testUserData } from '../globalSetup'
 import { prisma } from '../../prisma/client'
 
+beforeAll(async () => {
+  const userData = testUserData.admin.dni
+
+  const testUser = await prisma.user.findUnique({
+    where: { dni: userData },
+  })
+
+  const resourceData = [
+    {
+      title: 'test-resource-1-blog',
+      slug: 'test-resource-1-blog',
+      url: 'https://sample.com',
+      userId: testUser?.id,
+      resourceType: 'BLOG',
+    },
+    {
+      title: 'test-resource-2-video',
+      slug: 'test-resource-2-video',
+      url: 'https://sample.com',
+      userId: testUser?.id,
+      resourceType: 'VIDEO',
+    },
+  ]
+
+  const resources = await prisma.$transaction(
+    resourceData.map((resource) => prisma.resource.create({ data: resource }))
+  )
+
+const favoritesData = resources.map(resource => ({
+  topicId: testTopic.id,
+  resourceId: resource.id
+}))
+
+  // create favorites for each user
+  await prisma.favorites.createMany({
+    data:favoritesData
+  })
+})
+
+afterAll(async () => {
+  await prisma.favorites.deleteMany({
+    where: {},
+  })
+  await prisma.resource.delete({
+    where: {},
+  })
+})
+
 describe('Testing /favorites/ endpoint', () => {
-    describe('Testing GET /by-user/:userId', () => {
-        test('Should respond OK status and return favorites as an array.', async () => {
+  describe('Testing GET /by-user/:userId', () => {
+    test('Should respond OK status and return favorites as an array.', async () => {
+      const user = await prisma.user.findUnique({
+        where: {
+          email: 'registered@registered.com',
+        },
+      })
+      const userId = user?.id
+      const response = await supertest(server).get(
+        `/api/v1/favorites/by-user/${userId}`
+      )
 
-            const user = await prisma.user.findUnique({
-                where: {
-                    email: "registered@registered.com"
-                },
-            });
-            const userId = user?.id
-            const response = await supertest(server).get(`/api/v1/favorites/by-user/${userId}`)
-
-            expect(response.status).toBe(200);
-            // expect(response.body).toBeInstanceOf(Array)
-            // expect(response.body.length).toBeGreaterThan(0)
-            expect(response.body).toEqual(
-                expect.arrayContaining([
-                    expect.objectContaining({
-                        id: expect.any(String),
-                        name: expect.any(String)
-                    })
-                ])
-            )
-        })
+      expect(response.status).toBe(200)
+      // expect(response.body).toBeInstanceOf(Array)
+      // expect(response.body.length).toBeGreaterThan(0)
+      expect(response.body).toEqual(
+        expect.arrayContaining([
+          expect.objectContaining({
+            id: expect.any(String),
+            name: expect.any(String),
+          }),
+        ])
+      )
     })
+  })
 })
--- back/src/routes/favoritesRouter.ts
+++ back/src/routes/favoritesRouter.ts
@@ -1,14 +1,11 @@
 import Router from '@koa/router'
-import { getUserFavoriteResources} from '../controllers'
+import { getUserFavoriteResources } from '../controllers'
 import { pathRoot } from './routes'
 
 const favoritesRouter = new Router()
 
 favoritesRouter.prefix(pathRoot.v1.favorites)
 
-favoritesRouter.get(
-  '/by-user/:userId',
-  getUserFavoriteResources
-)
+favoritesRouter.get('/by-user/:userId', getUserFavoriteResources)
 
 export { favoritesRouter }
