69708bb18fbaced0fd8c2943dc0a42b45ecd272b
Halimeh <101599560+Lahali@users.noreply.github.com>
2023-05-21 21:30:07 +0200

    238-home-without-select-category

--- front/src/pages/Home.tsx
+++ front/src/pages/Home.tsx
@@ -2,97 +2,17 @@ import styled from 'styled-components'
 import { FC } from 'react'
 import icons from '../assets/icons'
 import { FlexBox, colors, device, dimensions, font } from '../styles'
-import { CardHome, InputGroup } from '../components/molecules'
-import {
-  HowToHelpCard,
-  CategoriesList,
-  ResourcesList,
-} from '../components/organisms'
+import { CardHome } from '../components/molecules'
+import { CategoriesList } from '../components/organisms'
 import { Title, Text, Icon } from '../components/atoms'
 
-export type TResource = {
-  id: string
-  title: string
-  createdBy: string
-  createdOn: string
-  description: string
-  img: string
-  url: string
-  likes: number
-}
-
-export const resources: TResource[] = [
-  {
-    id: 'resourceId1',
-    title: 'JavaScript en 45 segundos!',
-    createdBy: 'Ona Costa',
-    createdOn: '1995-12-17T03:07:00',
-    description: 'Proyecto práctico',
-    img: icons.profileAvatar,
-    url: 'https://www.google.com/search?q=link1',
-    likes: 5,
-  },
-  {
-    id: 'resourceId2',
-    title: 'REST API de cero a  ninja!',
-    createdBy: 'Ona Costa',
-    createdOn: '1995-12-17T03:07:00',
-    description: 'Teoria con ejemplos',
-    img: icons.profileAvatar,
-    url: 'https://www.google.com/search?q=link2',
-    likes: 22,
-  },
-  {
-    id: 'resourceId3',
-    title: 'Context en 5 minutos!',
-    createdBy: 'Ona Costa',
-    createdOn: '1995-12-17T03:07:00',
-    description: 'Teoria con ejemplos',
-    img: icons.profileAvatar,
-    url: 'https://www.google.com/search?q=link3',
-    likes: 56,
-  },
-  {
-    id: 'resourceId4',
-    title: 'Redux para principiantes!',
-    createdBy: 'Ona Costa',
-    createdOn: '1995-12-17T03:07:00',
-    description: 'Teoria con ejemplos',
-    img: icons.profileAvatar,
-    url: 'https://www.google.com/search?q=link4',
-    likes: 125,
-  },
-]
-
-// TODO: mobile first!
-export const MobileStyled = styled.div`
-  display: block;
-  @media only ${device.Laptop} {
-    display: none;
-  }
-`
 export const DesktopStyled = styled.div`
   display: none;
   @media only ${device.Laptop} {
     display: block;
   }
 `
-// Style Mobile
-const HeaderContainerStyled = styled(FlexBox)`
-  padding: ${dimensions.spacing.lg} ${dimensions.spacing.lg} 0
-    ${dimensions.spacing.lg};
-  align-items: stretch;
-  gap: ${dimensions.spacing.base};
-`
-const GrayBackground = styled.div`
-  z-index: -9999;
-  position: absolute;
-  width: 100%;
-  height: 155px;
-  background-color: ${colors.gray.gray5};
-`
 
-// style Desktop
 const MainContainer = styled.div`
   display: flex;
   justify-content: flex-start;
@@ -141,58 +61,38 @@ const cardHomeContent = [
 ]
 
 const Home: FC = () => (
-  <>
-    <MobileStyled>
-      <GrayBackground />
-      <HeaderContainerStyled>
-        <HowToHelpCard />
-        <InputGroup
-          label="Buscar"
-          placeholder="¿Buscas un tema en concreto?"
-          id="search"
-          name="search"
-          icon="search"
-          color={colors.gray.gray3}
-        />
-      </HeaderContainerStyled>
+  <DesktopStyled>
+    <MainContainer>
       <CategoriesList />
-      <ResourcesList title="Recursos que te gustan" resources={resources} />
-      <ResourcesList title="Tus recursos" resources={resources} />
-    </MobileStyled>
-    <DesktopStyled>
-      <MainContainer>
-        <CategoriesList />
-        {/* ==> DESKTOP HOME */}
-        <DivStyled>
-          <TextContainerStyled>
-            <Title as="h1">¡Bienvenid@ a la wiki de la IT Academy!</Title>
-            <Text color={`${colors.gray.gray3}`} fontSize={`${font.xs}`}>
-              Funcionalidades básicas que te ofrece esta plataforma
-            </Text>
-          </TextContainerStyled>
-          <FlexBox direction="row">
-            {cardHomeContent.map((content) => (
-              <CardHome
-                key={content.id}
-                cardTitle={content.title}
-                cardSubtitle={content.subtitle}
-                indicator={content.indicator}
-                icon={content.icon}
-                data-testid="cardHome"
-              />
-            ))}
-          </FlexBox>
-          <TextContainerStyled direction="row">
-            <Icon name="info" fill={0} color={`${colors.gray.gray3}`} />
-            <Text color={`${colors.gray.gray3}`} fontSize={`${font.xss}`}>
-              Para comenzar a visualizar recursos, selecciona una categoría.
-              Registro necesario para subir y votar recursos
-            </Text>
-          </TextContainerStyled>
-        </DivStyled>
-      </MainContainer>
-    </DesktopStyled>
-  </>
+      <DivStyled>
+        <TextContainerStyled>
+          <Title as="h1">¡Bienvenid@ a la wiki de la IT Academy!</Title>
+          <Text color={`${colors.gray.gray3}`} fontSize={`${font.xs}`}>
+            Funcionalidades básicas que te ofrece esta plataforma
+          </Text>
+        </TextContainerStyled>
+        <FlexBox direction="row">
+          {cardHomeContent.map((content) => (
+            <CardHome
+              key={content.id}
+              cardTitle={content.title}
+              cardSubtitle={content.subtitle}
+              indicator={content.indicator}
+              icon={content.icon}
+              data-testid="cardHome"
+            />
+          ))}
+        </FlexBox>
+        <TextContainerStyled direction="row">
+          <Icon name="info" fill={0} color={`${colors.gray.gray3}`} />
+          <Text color={`${colors.gray.gray3}`} fontSize={`${font.xss}`}>
+            Para comenzar a visualizar recursos, selecciona una categoría.
+            Registro necesario para subir y votar recursos
+          </Text>
+        </TextContainerStyled>
+      </DivStyled>
+    </MainContainer>
+  </DesktopStyled>
 )
 
 export { Home }
