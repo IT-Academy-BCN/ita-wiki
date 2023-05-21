d38a8491f3071eec71035e6e87b096ac8f0037ef
Halimeh <101599560+Lahali@users.noreply.github.com>
2023-05-21 21:33:47 +0200

    238-fixed error with tests

--- front/src/__tests__/pages/Home.test.tsx
+++ front/src/__tests__/pages/Home.test.tsx
@@ -6,11 +6,6 @@ describe('Home page', () => {
   it('Renders correctly', () => {
     render(<Home />)
 
-    expect(screen.getByLabelText('Buscar')).toBeInTheDocument()
-    expect(screen.getByText('¿Cómo colaborar en la wiki?')).toBeInTheDocument()
-    expect(screen.getByText('Recursos que te gustan')).toBeInTheDocument()
-    expect(screen.getByText('Tus recursos')).toBeInTheDocument()
-
     expect(screen.getByText(/vota los recursos/i)).toBeInTheDocument()
     expect(
       screen.getByText(/¡Bienvenid@ a la wiki de la IT Academy!/)
