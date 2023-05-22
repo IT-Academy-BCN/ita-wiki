ba81d6f18d676c75682b227e619a0dad68fca06c
Vanessa Palazón <94196643+missvader@users.noreply.github.com>
2023-05-22 11:57:14 +0200

    Update Navbar.test.tsx

--- front/src/__tests__/organisms/Navbar.test.tsx
+++ front/src/__tests__/organisms/Navbar.test.tsx
@@ -1,16 +1,10 @@
-import { render, screen } from '@testing-library/react';
-import { BrowserRouter } from 'react-router-dom';
+import { render, screen } from "../test-utils";
 import { Navbar } from "../../components/organisms";
-import { MobileStyled } from "../../pages/Home";
 
 describe('Navbar', () => {
   it('renders correctly', () => {
     render(
-      <BrowserRouter>
-        <MobileStyled>
           <Navbar title="Wiki" />
-        </MobileStyled>
-      </BrowserRouter>
     );
 
     const titleElement = screen.getByText('Wiki');
