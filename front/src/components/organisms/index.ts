db4ddc75f9186735c383e0fc8d154821242e5047
Vincenzo- <viulenzgg@gmail.com>
2023-05-23 13:09:02 +0200

    merged main into branch

--- front/src/components/organisms/index.ts
+++ front/src/components/organisms/index.ts
@@ -1,6 +1,13 @@
 import { CategoriesList } from './CategoriesList'
 import { HowToHelpCard } from './HowToHelpCard'
 import { ResourcesList } from './ResourcesList'
-import {ResourceForm} from './ResourceForm'
+import { ResourceForm } from './ResourceForm'
+import { TopicsRadioWidget } from './TopicsRadioWidget'
 
-export { ResourcesList, HowToHelpCard, CategoriesList, ResourceForm }
+export {
+  ResourcesList,
+  HowToHelpCard,
+  CategoriesList,
+  ResourceForm,
+  TopicsRadioWidget,
+}
