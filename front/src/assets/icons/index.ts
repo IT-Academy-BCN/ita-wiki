1ebc06377859d595c990c41e60ced2095b65cecf
Halimeh <101599560+Lahali@users.noreply.github.com>
2023-05-23 17:11:03 +0200

    263-reviewed lint errors

--- front/src/assets/icons/index.ts
+++ front/src/assets/icons/index.ts
@@ -3,18 +3,18 @@ import arrowDown from './arrow-down.svg'
 import chevronDown from './chevron-down.svg'
 import dataScience from './data-science.svg'
 import editPen from './edit-pen.svg'
+import itLogo from './it-logo.svg'
+import java from './java.svg'
 import javascript from './javascript.svg'
 import lockDynamic from './lock-dynamic-color.svg'
 import newFolder from './new-folder-dynamic-color.svg'
-import profileAvatar from './profile-avatar.svg'
-import react from './react.svg'
-import vue from './vue.svg'
 import node from './nodejs-logo.svg'
 import php from './php.svg'
-import java from './java.svg'
+import profileAvatar from './profile-avatar.svg'
 import puzzleDynamic from './puzzle-dynamic-color.svg'
+import react from './react.svg'
 import thumbUp from './thumb-up-dynamic-color.svg'
-import itLogo from './it-logo.svg'
+import vue from './vue.svg'
 
 const icons = {
   angular,
@@ -22,18 +22,18 @@ const icons = {
   chevronDown,
   dataScience,
   editPen,
+  itLogo,
+  java,
   javascript,
   lockDynamic,
   newFolder,
-  profileAvatar,
-  react,
-  vue,
   node,
   php,
-  java,
+  profileAvatar,
   puzzleDynamic,
+  react,
   thumbUp,
-  itLogo
+  vue,
 }
 
 export default icons
