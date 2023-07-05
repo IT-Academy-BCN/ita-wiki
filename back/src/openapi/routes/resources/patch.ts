1e875b81418d10bdae07c641eba107ce00410a9c
David Iglesias <im.david85@gmail.com>
2023-07-05 10:32:52 +0200

    swagger

---
+++ back/src/openapi/routes/resources/patch.ts
@@ -0,0 +1,36 @@
+import { pathRoot } from '../../../routes/routes'
+import { resourcePatchSchema } from '../../../schemas/resource/resourcePatchSchema'
+import { cookieAuth } from '../../components/cookieAuth'
+import {
+  invalidTokenResponse,
+  missingTokenResponse,
+} from '../../components/responses/authMiddleware'
+import { registry } from '../../registry'
+
+registry.registerPath({
+  method: 'patch',
+  tags: ['resources'],
+  path: `${pathRoot.v1.resources}`,
+  description: 'Allows a logged in user to modify his resources.',
+  summary: 'Patch a resource',
+  security: [{ [cookieAuth.name]: [] }],
+  request: {
+    body: {
+      content: {
+        'application/json': {
+          schema: resourcePatchSchema,
+        },
+      },
+    },
+  },
+  responses: {
+    204: {
+      description: 'Resource has been modified',
+    },
+    401: missingTokenResponse,
+    405: invalidTokenResponse,
+    404: {
+      description: 'Not found',
+    },
+  },
+})
