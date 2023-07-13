a5fa95832a013d7fd0d56b97cf4c6adc6651b870
Halimeh <101599560+Lahali@users.noreply.github.com>
2023-07-13 10:08:35 +0200

    408-updated layout and started tests

--- front/src/pages/index.ts
+++ front/src/pages/index.ts
@@ -4,5 +4,6 @@ import { Information } from './Information'
 import ErrorPage from './ErrorPage'
 import Login from '../components/organisms/Login'
 import Register from '../components/organisms/Register'
+import NewCategory from './NewCategory'
 
-export { Category, Home, Login, Register, ErrorPage, Information }
+export { Category, Home, Login, Register, ErrorPage, Information, NewCategory }
