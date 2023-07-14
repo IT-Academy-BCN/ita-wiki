fe7766851945fa4f966cfdcd8013b55e951af1bf
versalitas <caprichismo@gmail.com>
2023-07-14 11:00:00 +0200

    add authorize middleware (separated by concern), rename authenticate and error handling clean up

--- back/src/middleware/errorMiddleware.ts
+++ back/src/middleware/errorMiddleware.ts
@@ -1,6 +1,8 @@
 /* eslint-disable no-ex-assign */
 import { Context, Next } from 'koa'
 import { ZodError } from 'zod'
+// eslint-disable-next-line prettier/prettier
+import jwt from 'jsonwebtoken'
 import { UnauthorizedError, ValidationError } from '../helpers/errors'
 
 const errorMiddleware = async (ctx: Context, next: Next) => {
@@ -9,6 +11,9 @@ const errorMiddleware = async (ctx: Context, next: Next) => {
   } catch (error: any) {
     if (error instanceof ZodError) {
       error = new ValidationError(error.errors)
+    } else if (error instanceof jwt.JsonWebTokenError) {
+      ctx.status = 405
+      ctx.body = { error: 'Token is not valid' }
     } else if (error?.errorInfo?.code === 'auth/id-token-expired') {
       error = new UnauthorizedError('refresh_token')
     }
