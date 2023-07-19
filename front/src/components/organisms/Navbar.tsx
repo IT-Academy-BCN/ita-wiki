b4744930fdaa7153c6c215815cc97a5f1bc4c7a5
Vincenzo Cristiano <viulenzgg@gmail.com>
2023-07-19 09:23:49 +0200

    Update Navbar.tsx

--- front/src/components/organisms/Navbar.tsx
+++ front/src/components/organisms/Navbar.tsx
@@ -4,7 +4,6 @@ import { FlexBox, colors, dimensions } from '../../styles'
 import { Title } from '../atoms'
 import { SelectLanguage } from '../molecules/SelectLanguage'
 import { UserButton } from '../molecules/UserButton'
-// eslint-disable-next-line import/no-cycle
 import { CategoriesList } from './CategoriesList'
 import closeButton from '../../assets/icons/x.svg'
 import PlusImg from '../../assets/icons/plus.svg'
@@ -105,7 +104,6 @@ const MenuItems = styled(FlexBox)`
   }
 `
 
-
 export const Navbar: React.FC = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false)
 
@@ -116,11 +114,7 @@ export const Navbar: React.FC = () => {
   return (
     <NavbarStyled direction="row">
       <HamburgerMenu src={MenuHamburger} alt="menu" onClick={toggleMenu} />
-      <ButtonImg
-        data-testid="new-post-button"
-        src={PlusImg}
-        alt="newPost"
-      />
+      <ButtonImg data-testid="new-post-button" src={PlusImg} alt="newPost" />
       <LangDesktop>
         <SelectLanguage />
       </LangDesktop>
