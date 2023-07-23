66cf4922d07e2248e6bf50eee6f0e3dbde4d476e
JuanMa <mitild82@gmail.com>
2023-07-23 10:57:42 +0200

    changed useGetFavorites hook and userProfile page accordingly

--- front/src/hooks/useGetFavorites.tsx
+++ front/src/hooks/useGetFavorites.tsx
@@ -1,28 +1,39 @@
 import { useQuery } from '@tanstack/react-query'
 import { urls } from '../constants'
-import { Spinner } from '../components/atoms'
+
+export type TFavorites = {
+  id: string
+  title: string
+  slug: string
+  description: string
+  url: string
+  resourceType: string
+  userId: string
+  createdAt: string
+  updatedAt: string
+}
+
+const getFavorites = async (): Promise<TFavorites[] | Error> => {
+  try {
+    const response: Response = await fetch(urls.getFavorites)
+
+      if (!response.ok) {
+        throw new Error("Error fetching favorite resources")
+      }
+      const data: TFavorites[] = await response.json()
+      return data
+  }
+  catch (error) {
+    throw new Error("Error fetching favorite resources")
+  }
+}
 
 export const useGetFavorites = () => {
-  const getFavorites = async () =>
-    fetch(urls.getFavorites)
-      .then((res) => res.json())
-  
-    const {
-      error,
-      data,
-      status
-    } = useQuery({
-      queryKey: ["userFavorites"],
-      queryFn: getFavorites
-    })
-  
-    if(status === 'loading') {
-      return <Spinner />
-    }
-  
-    if(status === 'error') {
-      throw new Error(`Error fetching user's favorites' ${error}`)
-    }
 
-    return data
+  const data = useQuery<TFavorites[] | Error> ({
+    queryKey: ["userFavorites"],
+    queryFn: getFavorites
+  })
+  
+  return data
 }
--- front/src/pages/UserProfile.tsx
+++ front/src/pages/UserProfile.tsx
@@ -4,13 +4,15 @@ import { CardProfile } from '../components/molecules/CardProfile'
 import icons from '../assets/icons'
 import { paths } from '../constants'
 import { logOut } from '../utils/logOut'
-import { useGetFavorites } from '../hooks/useGetFavorites'
+import {useGetFavorites, TFavorites} from '../hooks/useGetFavorites';
 import { useGetMyResources } from '../hooks/useGetMyResources'
 import { useAuth } from '../context/AuthProvider'
 import { BackButton } from '../components/atoms/BackButton'
 
 export const UserProfile: FC = () => {
   const favorites = useGetFavorites()
+  const favoritesData = favorites.data as TFavorites[] | undefined;
+  const favoritesLength = favoritesData?.length ?? 0;
   const { resources } = useGetMyResources()
   const { user } = useAuth()
   const navigate = useNavigate()
@@ -40,7 +42,7 @@ export const UserProfile: FC = () => {
         email={ userWithEmail?.email ?? 'user@user.com' }
         contributions={ resources?.length ?? 0 } 
         votes={ totalVotes }
-        favorites={ favorites?.length ?? 0 }
+        favorites={ favoritesLength }
         handleLogOut={ () => logOut(navigate, paths.home) }
       />
     </>
