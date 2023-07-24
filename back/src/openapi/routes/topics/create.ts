07c5578340635f1381851aa9b3c44334a3e014f8
Alex Orpi <55232729+alex0rpi@users.noreply.github.com>
2023-07-24 09:34:04 +0200

    423-be-add-topics-patch-create-endpnts-to-swagger (#440)

---
+++ back/src/openapi/routes/topics/create.ts
@@ -0,0 +1,47 @@
+import { z } from 'zod'
+import { pathRoot } from '../../../routes/routes'
+import { topicCreateSchema } from '../../../schemas'
+import { cookieAuth } from '../../components/cookieAuth'
+import {
+  invalidTokenResponse,
+  missingTokenResponse,
+} from '../../components/responses/authMiddleware'
+import { deniedAccessResponse } from '../../components/responses/authorize'
+import { registry } from '../../registry'
+
+registry.registerPath({
+  method: 'post',
+  tags: ['topics'],
+  path: `${pathRoot.v1.topics}`,
+  description:
+    'Creates a new topic. The requestor has to be logged in and with role MENTOR or higher.',
+  summary: 'Creates a new topic.',
+  security: [{ [cookieAuth.name]: [] }],
+  request: {
+    body: {
+      content: {
+        'application/json': {
+          schema: topicCreateSchema,
+        },
+      },
+    },
+  },
+  responses: {
+    204: {
+      description: 'New topic created succesfully.',
+    },
+    401: missingTokenResponse,
+    403: deniedAccessResponse,
+    405: invalidTokenResponse,
+    500: {
+      description: 'Other error',
+      content: {
+        'application/json': {
+          schema: z.object({
+            message: z.string().openapi({ example: 'Database error' }),
+          }),
+        },
+      },
+    },
+  },
+})
