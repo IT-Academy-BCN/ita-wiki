628bfcd94accb15bcf87d03044ceaf35a11f4675
JuanMa <mitild82@gmail.com>
2023-07-24 10:21:09 +0200

    test run on useGetFavorites hook

--- front/src/__tests__/hooks/useGetResources.test.tsx
+++ front/src/__tests__/hooks/useGetResources.test.tsx
@@ -1,10 +1,10 @@
 import { QueryClientProvider } from "@tanstack/react-query"
 import { renderHook, waitFor } from "../test-utils"
 import { queryClient } from '../setup'
-import { useGetResources } from "../../hooks"
+import { useGetResources } from '../../hooks'
 
-describe('useGetResources', () => {
-  it("gets an object with user's resources", async () => {
+describe('useGetFavorites', () => {
+  it("gets an array of user's favorite resources", async () => {
     const wrapper = ({ children }: { children: React.ReactNode }) => (
       <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
     )
