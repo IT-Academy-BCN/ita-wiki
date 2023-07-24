e642ec523ab2fe996ee221a3a00168cecd5492f9
Kevin Mamaqi <kevinmamaqi@gmail.com>
2023-07-24 09:28:13 +0200

    update;

--- front/src/__tests__/hooks/useGetFavorites.test.tsx
+++ front/src/__tests__/hooks/useGetFavorites.test.tsx
@@ -1,7 +1,9 @@
-import { setupServer } from 'msw/node';
-import { renderHook } from "../test-utils"
-import { handlers } from '../../__mocks__/handlers';
-import { useGetFavorites } from "../../hooks"
+import { setupServer } from 'msw/node'
+import { QueryClientProvider } from '@tanstack/react-query'
+import { waitFor, renderHook } from '../test-utils'
+import { handlers } from '../../__mocks__/handlers'
+import { useGetFavorites } from '../../hooks'
+import { queryClient } from '../setup'
 
 const server = setupServer(...handlers)
 
@@ -13,7 +15,12 @@ afterAll(() => server.close())
 
 describe('useGetFavorites', () => {
   it("gets an array of user's favorite resources", async () => {
-    const { result } = renderHook(useGetFavorites)
-    expect(result.current.data).toHaveLength(0)
+    const wrapper = ({ children }: { children: React.ReactNode }) => (
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    )
+    const { result } = renderHook(() => useGetFavorites(), { wrapper })
+    await waitFor(() => expect(result.current.isSuccess).toBeTruthy())
+
+    expect(result.current.data).toHaveLength(1)
   })
 })
