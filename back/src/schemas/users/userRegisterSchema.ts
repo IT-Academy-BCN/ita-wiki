9d67ed843a1cb832f242d09dd218353d67b9fedd
Danny <dannym.mejia@gmail.com>
2023-09-19 08:14:19 +0200

    474-be-bug-registration-form

--- back/src/schemas/users/userRegisterSchema.ts
+++ back/src/schemas/users/userRegisterSchema.ts
@@ -5,10 +5,18 @@ import { passwordRegex } from '../regex/passwordRegex'
 export const userRegisterSchema = userSchema
   .pick({
     email: true,
-    name: true,
     dni: true,
     specialization: true,
   })
   .extend({
+    name: z.string(),
+    accept: z.literal<boolean>(true),
     password: z.string().min(8).regex(passwordRegex),
+    confirmPassword: z.string().min(8).regex(passwordRegex),
   })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: 'Passwords must match',
+    path: ['confirmPassword'],
+  })
+
+export type UserRegister = z.infer<typeof userRegisterSchema>
