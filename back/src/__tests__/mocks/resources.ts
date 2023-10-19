e20627932e2457e9c6c37d401661cb828cab71df
SergiFont <sfontsmx2010@gmail.com>
2023-10-19 16:44:05 +0200

    test review

--- back/src/__tests__/mocks/resources.ts
+++ back/src/__tests__/mocks/resources.ts
@@ -2,7 +2,7 @@ import { Prisma } from '@prisma/client'
 
 export const resourceTestData: Omit<
   Prisma.ResourceCreateArgs['data'],
-  'userId'
+  'userId' | 'categoryId'
 >[] = [
   {
     title: 'test-resource-1-blog',
@@ -10,8 +10,6 @@ export const resourceTestData: Omit<
     description: 'Lorem ipsum blog',
     url: 'https://sample.com',
     resourceType: 'BLOG',
-    status: 'SEEN',
-    categoryId: '',
   },
   {
     title: 'test-resource-2-video',
@@ -19,8 +17,6 @@ export const resourceTestData: Omit<
     description: 'Lorem ipsum video',
     url: 'https://sample.com',
     resourceType: 'VIDEO',
-    status: 'NOT_SEEN',
-    categoryId: '',
   },
   {
     title: 'test-resource-3-tutorial',
@@ -28,7 +24,5 @@ export const resourceTestData: Omit<
     description: 'Lorem ipsum tutorial',
     url: 'https://sample.com',
     resourceType: 'TUTORIAL',
-    status: 'NOT_SEEN',
-    categoryId: '',
   },
 ]
