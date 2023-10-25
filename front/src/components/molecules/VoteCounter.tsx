0270dff920a6bb3299c1ab0743345cc9a85105bc
Vincenzo- <viulenzgg@gmail.com>
2023-10-25 12:55:17 +0200

    created and tested updateCachedVoteCount helper function

--- front/src/components/molecules/VoteCounter.tsx
+++ front/src/components/molecules/VoteCounter.tsx
@@ -6,6 +6,7 @@ import { Icon, Text } from '../atoms'
 import { urls } from '../../constants'
 import { useAuth } from '../../context/AuthProvider'
 import { TResource } from '../../context/store/types'
+import { updateCachedVoteCount } from '../../helpers'
 
 type ArrowProp = {
   color: string
@@ -41,80 +42,7 @@ type TVoteMutationData = {
   vote: TUserVote
 }
 
-const getDownvote = (
-  currUserVote: number,
-  currDownvote: number,
-  currVote: TUserVote
-) => {
-  if (
-    (currUserVote === 0 && currVote === 'up') ||
-    (currUserVote === 1 && currVote === 'cancel')
-  )
-    return currDownvote
-  if (
-    (currUserVote === 0 && currVote === 'down') ||
-    (currUserVote === 1 && currVote === 'down')
-  )
-    return currDownvote + 1
-  return currDownvote - 1
-}
 
-const getUpvote = (
-  currUserVote: number,
-  currUpvote: number,
-  currVote: TUserVote
-) => {
-  if (
-    (currUserVote === 0 && currVote === 'down') ||
-    (currUserVote === -1 && currVote === 'cancel')
-  )
-    return currUpvote
-  if (
-    (currUserVote === 0 && currVote === 'up') ||
-    (currUserVote === -1 && currVote === 'up')
-  )
-    return currUpvote + 1
-  return currUpvote - 1
-}
-
-const getTotal = (
-  currUserVote: number,
-  currTotal: number,
-  currVote: TUserVote
-) => {
-  if (
-    (currUserVote === 0 && currVote === 'up') ||
-    (currUserVote === -1 && currVote === 'cancel')
-  )
-    return currTotal + 1
-  if (
-    (currUserVote === 1 && currVote === 'cancel') ||
-    (currUserVote === 0 && currVote === 'down')
-  )
-    return currTotal - 1
-  if (currUserVote === 1 && currVote === 'down') return currTotal - 2
-  return currTotal + 2
-}
-
-const getUserVote = (currVote: TUserVote) => {
-  if (currVote === 'up') return 1
-  if (currVote === 'down') return -1
-  return 0
-}
-
-const updateCachedVoteCount = (
-  currentVoteCount: TVoteCount,
-  vote: TUserVote
-) => {
-  const { downvote, upvote, total, userVote } = currentVoteCount
-
-  return {
-    downvote: getDownvote(userVote, downvote, vote),
-    upvote: getUpvote(userVote, upvote, vote),
-    total: getTotal(userVote, total, vote),
-    userVote: getUserVote(vote),
-  }
-}
 
 const updateVote = async ({ resourceId, vote }: TVoteMutationData) => {
   const response = await fetch(urls.vote, {
--- front/src/helpers/index.ts
+++ front/src/helpers/index.ts
@@ -1,3 +1,4 @@
 export { buildQueryString } from './filters'
 export type { TFilters } from './filters'
 export { getCategories } from './fetchers'
+export {updateCachedVoteCount} from "./updateCachedVoteCount"
