aa9a2d1e1a419862cd3b7851a5dc90adc276341e
Alex Orpi <orpi.alex@hotmail.com>
2023-11-02 16:00:35 +0100

    596-be-add-topics-to-getfavorites-response

--- back/src/schemas/resource/resourceFavoriteSchema.ts
+++ back/src/schemas/resource/resourceFavoriteSchema.ts
@@ -1,7 +1,10 @@
 // import { userSchema } from '../users/userSchema'
+import { z } from 'zod'
+import { topicSchema } from '../topic/topicSchema'
 import { voteCountSchema } from '../voteCountSchema'
 import { resourceSchema } from './resourceSchema'
 
 export const resourceFavoriteSchema = resourceSchema.extend({
   voteCount: voteCountSchema,
+  topics: z.array(z.object({ topic: topicSchema })),
 })
