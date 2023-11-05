b42081a1fcd227a860244aca8ccce0bbbe156638
JuditKaramazov <j.lazaromoyano7@gmail.com>
2023-11-05 22:47:55 +0100

    Implement EN, ES and CAT translations

--- front/src/components/organisms/UsersManager.tsx
+++ front/src/components/organisms/UsersManager.tsx
@@ -1,13 +1,19 @@
 import { useAuth } from '../../context/AuthProvider'
+import { useTranslation } from 'react-i18next'
 import { AccountAdmin } from '../../pages/AccountAdim'
 
 const UsersManager = () => {
   const { user } = useAuth()
 
+  const { t } = useTranslation()
+
   return (
     <>
-      Users Manager
+      {t('Administrador de Usuarios')}
       {user?.role === 'ADMIN' && <AccountAdmin />}
+      {user?.role === 'MENTOR' && (
+        <p>{t("No tienes permisos suficientes para acceder al contenido.")}</p>
+      )}
     </>
   )
 }
