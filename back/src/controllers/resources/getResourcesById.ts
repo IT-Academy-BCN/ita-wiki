3660bd7eacc3942fc5b4898c243bdea0c312269c
Alex Orpi <orpi.alex@hotmail.com>
2023-11-09 06:48:53 +0100

    610-be-getresourcesbyid-wrong-uservote-isfavorite

--- back/src/controllers/resources/getResourcesById.ts
+++ back/src/controllers/resources/getResourcesById.ts
@@ -1,4 +1,5 @@
 import Koa, { Middleware } from 'koa'
+import { User } from '@prisma/client'
 import { prisma } from '../../prisma/client'
 import { transformResourceToAPI } from '../../helpers/transformResourceToAPI'
 import { resourceGetSchema } from '../../schemas'
@@ -6,21 +7,29 @@ import { NotFoundError } from '../../helpers/errors'
 
 export const getResourcesById: Middleware = async (ctx: Koa.Context) => {
   const { resourceId } = ctx.params
+  const user = ctx.user as User | null
+  const voteSelect =
+    ctx.user !== null ? { userId: true, vote: true } : { vote: true }
+
   const resource = await prisma.resource.findUnique({
     where: { id: resourceId },
     include: {
       user: {
         select: {
+          id: true,
           name: true,
         },
       },
-      vote: { select: { vote: true } },
+      vote: { select: voteSelect },
       topics: { select: { topic: true } },
     },
   })
   if (!resource) throw new NotFoundError('Resource not found')
 
-  const resourceWithVote = transformResourceToAPI(resource)
+  const resourceWithVote = transformResourceToAPI(
+    resource,
+    user ? user.id : undefined
+  )
   const parsedResource = resourceGetSchema.parse(resourceWithVote)
   ctx.status = 200
   ctx.body = parsedResource
