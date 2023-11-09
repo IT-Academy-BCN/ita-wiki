b284bd9bbede07796809f0d1f9329d729440a48e
Judit Karamazov <j.lazaromoyano7@gmail.com>
2023-11-09 23:30:04 +0100

    Update translations & create VotesDate component

--- front/src/__tests__/pages/Category.test.tsx
+++ front/src/__tests__/pages/Category.test.tsx
@@ -1,8 +1,6 @@
 import { expect, vi } from 'vitest'
-import { render, renderHook, screen, fireEvent, waitFor } from '../test-utils'
+import { render, screen, fireEvent, waitFor } from '../test-utils'
 import { Category } from '../../pages'
-import { useSortByDate } from '../../hooks/useSortByDate'
-import { useSortByVotes } from '../../hooks/useSortByVotes'
 import { TAuthContext, useAuth } from '../../context/AuthProvider'
 
 vi.mock('react-router-dom', async () => {
@@ -85,7 +83,7 @@ it('filters opens and closes correctly', () => {
 it('create new resource modal opens and closes correctly', () => {
   render(<Category />)
 
-  fireEvent.click(screen.getByText('+ Crear nuevo recurso'))
+  fireEvent.click(screen.getByText('+ Crear nou recurs'))
   expect(screen.getByText(/Nuevo Recurso/)).toBeInTheDocument()
 })
 
@@ -96,7 +94,7 @@ it('modal opens and closes correctly when user is not logged', async () => {
 
   render(<Category />)
 
-  fireEvent.click(screen.getByRole('button', { name: '+ Crear nuevo recurso' }))
+  fireEvent.click(screen.getByRole('button', { name: '+ Crear nou recurs' }))
   const modalTitle = screen.getByRole('heading', {
     name: /acceso restringido/i,
   })
@@ -111,7 +109,7 @@ it('modal opens and closes correctly when user is not logged', async () => {
 it('modal opens and closes correctly when user is logged', async () => {
   render(<Category />)
 
-  fireEvent.click(screen.getByRole('button', { name: '+ Crear nuevo recurso' }))
+  fireEvent.click(screen.getByRole('button', { name: '+ Crear nou recurs' }))
   const modalTitle = screen.getByRole('heading', {
     name: /nuevo recurso/i,
   })
@@ -143,85 +141,4 @@ it('status filter widget appears for users who are logged in', () => {
 
   const statusFilterWidget = screen.getByTestId('status-filter')
   expect(statusFilterWidget).toBeInTheDocument()
-})
-
-it('sorts resources by date in descending order', () => {
-  const items = [
-    { id: 1, date: '2023-11-01' },
-    { id: 2, date: '2023-10-30' },
-    { id: 3, date: '2023-10-28' },
-  ]
-
-  render(<Category />)
-
-  fireEvent.click(screen.getByText('Fecha'))
-
-  const { result } = renderHook(() => useSortByDate(items, 'date', 'desc'))
-
-  expect(result.current.sortedItems).toEqual([
-    { id: 1, date: '2023-11-01' },
-    { id: 2, date: '2023-10-30' },
-    { id: 3, date: '2023-10-28' },
-  ])
-})
-
-it('sorts resources by votes in ascending order', () => {
-  const votes = [
-    {
-      id: 'resource1',
-      title: 'Resource 1',
-      voteCount: {
-        downvote: 1,
-        upvote: 2,
-        total: 3,
-        userVote: 1,
-      },
-    },
-    {
-      id: 'resource2',
-      title: 'Resource 2',
-      voteCount: {
-        downvote: 0,
-        upvote: 0,
-        total: 0,
-        userVote: 0,
-      },
-    },
-    {
-      id: 'resource3',
-      title: 'Resource 3',
-      voteCount: {
-        downvote: 0,
-        upvote: 0,
-        total: 7,
-        userVote: 0,
-      },
-    },
-  ]
-
-  render(<Category />)
-
-  fireEvent.click(screen.getByText('Votos'))
-
-  const { result } = renderHook(() => useSortByVotes(votes, 'asc'))
-  const sortedResources = result.current.sortedVotes
-
-  const voteCounts = sortedResources.map((vote) => vote.voteCount.total)
-
-  expect(voteCounts).toEqual([0, 3, 7])
-})
-
-it('changes Votos and Fecha styles on click', () => {
-  render(<Category />)
-
-  const sortVotesButton = screen.getByText('Votos')
-  const sortDatesButton = screen.getByText('Fecha')
-
-  fireEvent.click(sortVotesButton)
-
-  expect(screen.getByText('Votos')).toHaveStyle('font-weight: bold')
-
-  fireEvent.click(sortDatesButton)
-
-  expect(screen.getByText('Fecha')).toHaveStyle('font-weight: bold')
 })
