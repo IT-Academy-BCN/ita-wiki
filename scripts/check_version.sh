fe5385aea2bca899d2f14895fbaf08c9856623c4
Kevin Mamaqi <kevinmamaqi@gmail.com>
2023-11-14 11:00:02 +0100

    update

--- scripts/check_version.sh
+++ scripts/check_version.sh
@@ -4,9 +4,12 @@ git fetch origin main:main
 VERSION_CURRENT=$(jq -r '.version' $PACKAGE_JSON_PATH)
 VERSION_MAIN=$(git show main:$PACKAGE_JSON_PATH | jq -r '.version')
 
-if [ "$(printf '%s\n' "$VERSION_MAIN" "$VERSION_CURRENT" | sort -V | head -n1)" != "$VERSION_MAIN" ]; then
-    echo "Version increased to $VERSION_CURRENT"
-else
-    echo "Version has not increased"
+if [ "$VERSION_CURRENT" = "$VERSION_MAIN" ]; then
+    echo "Version has not changed"
     exit 1
+elif [ "$(printf '%s\n' "$VERSION_MAIN" "$VERSION_CURRENT" | sort -V | head -n1)" = "$VERSION_CURRENT" ]; then
+    echo "Version has decreased"
+    exit 1
+else
+    echo "Version increased to $VERSION_CURRENT"
 fi
