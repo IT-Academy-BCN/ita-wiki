d33b0235bbbb04c8780663fa619b25a4ce8518c0
Alex Orpi <orpi.alex@hotmail.com>
2023-11-23 11:40:26 +0100

    688-be-fix-search-param-get-resources

--- back/src/controllers/resources/get.ts
+++ back/src/controllers/resources/get.ts
@@ -31,12 +31,13 @@ export const getResources: Middleware = async (ctx: Koa.Context) => {
     },
     resourceType: { in: resourceTypes },
     ...statusCondition,
-    ...(search && {
-      OR: [
-        { title: { contains: search, mode: 'insensitive' } },
-        { description: { contains: search, mode: 'insensitive' } },
-      ],
-    }),
+    ...(search &&
+      search.trim().length >= 2 && {
+        OR: [
+          { title: { contains: search, mode: 'insensitive' } },
+          { description: { contains: search, mode: 'insensitive' } },
+        ],
+      }),
   }
   const voteSelect =
     ctx.user !== null ? { userId: true, vote: true } : { vote: true }
