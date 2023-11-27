c69de9ef3696a57e32f4d89cf4373e96c8a45899
Judit Karamazov <125817426+JuditKaramazov@users.noreply.github.com>
2023-11-27 09:00:00 +0100

    665-fefix-category-page-details-without-resources (#696)
    * Update VotesDate visibility, translations & tests
    
    * Add optional filters & revert unnecessary changes
    
    * Fix no-unused-vars
    
    * Centralize VoteDate visibility's logic in Category

--- front/src/__tests__/organisms/VotesDateController.test.tsx
+++ front/src/__tests__/organisms/VotesDateController.test.tsx
@@ -1,10 +1,33 @@
 import { vi } from 'vitest'
 import { render, screen, fireEvent } from '../test-utils'
 import { VotesDateController } from '../../components/organisms'
+import { TAuthContext, useAuth } from '../../context/AuthProvider'
 
 const mockHandleSortOrder = vi.fn()
 const mockHandleSortByVotes = vi.fn()
 const mockHandleSortByDates = vi.fn()
+
+beforeEach(() => {
+  vi.mock('../../context/AuthProvider', async () => {
+    const actual = await vi.importActual('../../context/AuthProvider')
+    return {
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      ...actual,
+      useAuth: vi.fn(),
+    }
+  })
+  vi.mocked(useAuth).mockReturnValue({
+    user: {
+      name: 'TestName',
+      avatarId: 'TestAvatar',
+    },
+  } as TAuthContext)
+})
+
+afterEach(() => {
+  vi.restoreAllMocks()
+})
 
 describe('VotesDate component', () => {
   it('changes Votos and Fecha styles on click', () => {
@@ -14,18 +37,19 @@ describe('VotesDate component', () => {
         handleSortOrder={mockHandleSortOrder}
         handleSortByVotes={mockHandleSortByVotes}
         handleSortByDates={mockHandleSortByDates}
-      />)
-  
-    fireEvent.click(screen.getByText('Vots'))
-  
-    expect(screen.getByText('Vots')).toHaveStyle('font-weight: bold')
-    expect(screen.getByText('Data')).toHaveStyle('font-weight: normal')
+      />
+    )
+
+    fireEvent.click(screen.getByText(/vots/i))
+
+    expect(screen.getByText(/vots/i)).toHaveStyle('font-weight: bold')
+    expect(screen.getByText(/data/i)).toHaveStyle('font-weight: normal')
     expect(mockHandleSortByVotes).toHaveBeenCalledTimes(1)
-  
-    fireEvent.click(screen.getByText('Data'))
-  
-    expect(screen.getByText('Data')).toHaveStyle('font-weight: bold')
-    expect(screen.getByText('Vots')).toHaveStyle('font-weight: normal')
+
+    fireEvent.click(screen.getByText(/Data/i))
+
+    expect(screen.getByText(/data/i)).toHaveStyle('font-weight: bold')
+    expect(screen.getByText(/vots/i)).toHaveStyle('font-weight: normal')
     expect(mockHandleSortByDates).toHaveBeenCalledTimes(1)
   })
 })
