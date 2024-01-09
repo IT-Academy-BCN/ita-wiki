16bc9288498d48b222c18f2a7a0add99b6f67aff
Your Name <you@example.com>
2024-01-09 11:19:33 +0100

    migrate Radio component to ui

---
+++ packages/ui/src/components/atoms/Radio.tsx
@@ -0,0 +1,70 @@
+import { Ref, forwardRef } from 'react'
+import styled from 'styled-components'
+import { FlexBox, dimensions, colors, font } from '../../styles'
+import Label from './Label'
+
+type TRadioOptions = {
+  id: string
+  name: string
+}
+
+type TRadio = {
+  options: TRadioOptions[]
+  inputName: string
+  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void
+  hiddenLabel?: boolean
+  defaultChecked?: string
+}
+const RadioStyled = styled(FlexBox)`
+  ${FlexBox} {
+    margin-right: ${dimensions.spacing.xl};
+    > input {
+      width: 1.5rem;
+      height: 1.5rem;
+      min-width: 1.5rem;
+      min-height: 1.5rem;
+    }
+  }
+
+  ${Label} {
+    font-weight: ${font.regular};
+    color: ${colors.black.black3};
+    display: inline-block;
+    margin-left: ${dimensions.spacing.xxs} !important;
+    cursor: pointer;
+  }
+  accent-color: ${colors.primary};
+`
+
+const Radio = forwardRef(
+  (
+    {
+      options,
+      inputName,
+      hiddenLabel = false,
+      onChange,
+      defaultChecked,
+      ...rest
+    }: TRadio,
+    ref: Ref<HTMLInputElement>
+  ) => (
+    <RadioStyled {...rest} direction="row">
+      {options?.map(({ id, name }) => (
+        <FlexBox direction="row" key={id}>
+          <input
+            type="radio"
+            id={id}
+            value={id}
+            name={inputName}
+            ref={ref}
+            defaultChecked={defaultChecked === id}
+            onChange={onChange}
+          />
+          <Label htmlFor={id} text={name} hiddenLabel={hiddenLabel} />
+        </FlexBox>
+      ))}
+    </RadioStyled>
+  )
+)
+
+export default styled(Radio)``
--- packages/ui/src/components/atoms/index.ts
+++ packages/ui/src/components/atoms/index.ts
@@ -1,5 +1,6 @@
 import Input from './Input'
 import Label from './Label'
 import { Button } from './Button'
+import Radio from './Radio'
 
-export { Input, Label, Button }
+export { Input, Label, Button, Radio }
