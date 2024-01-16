193e1eccc0983fe1c570324debedf4acee6d072a
Your Name <you@example.com>
2024-01-16 11:23:01 +0100

    Update test + remove unnecessary comments

--- packages/ui/src/stories/atoms/Radio.stories.ts
+++ packages/ui/src/stories/atoms/Radio.stories.ts
@@ -2,17 +2,13 @@ import type { Meta, StoryObj } from '@storybook/react'
 
 import { Radio } from '../../components/atoms'
 
-// More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta = {
   title: 'Atoms/Radio',
   component: Radio,
   parameters: {
-    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
     layout: 'centered',
   },
-  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
   tags: ['autodocs'],
-  // More on argTypes: https://storybook.js.org/docs/api/argtypes
   argTypes: {
     options: [
       {
@@ -35,7 +31,6 @@ const meta = {
 export default meta
 type RadioStory = StoryObj<typeof meta>
 
-// More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
 export const DefaultRow: RadioStory = {
   args: {
     options: [
