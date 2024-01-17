657a01b29709f2374f60de3acd8974e75ca2c53a
Vanessa Palazón <94196643+missvader@users.noreply.github.com>
2024-01-17 12:56:55 +0100

    fe-sso_front-config (#818)

--- sso_front/vite.config.ts
+++ sso_front/vite.config.ts
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://dev.sso.itawiki.eurecatacademy.org/',
+        //target: 'http://localhost:8999/',
+
+        changeOrigin: true,
+        // rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
 })
