c21ef8f34df30905c10af86be7ba621234be0032
Maria Morales Reina <124587546+marmorrei@users.noreply.github.com>
2024-01-18 11:31:12 +0100

    834-fe-ui-inputgroup-use-default-values (#839)
    * add default values
    
    * update version

--- packages/ui/src/stories/molecules/InputGroup.stories.ts
+++ packages/ui/src/stories/molecules/InputGroup.stories.ts
@@ -1,55 +1,59 @@
 import type { Meta, StoryObj } from '@storybook/react'
 import { InputGroup } from '../../components/molecules'
 
-// More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta = {
   title: 'Molecules/InputGroup',
   component: InputGroup,
 
   parameters: {
-    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
     layout: 'centered',
   },
 
-  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
   tags: ['autodocs'],
-  // More on argTypes: https://storybook.js.org/docs/api/argtypes
-  argTypes: {
-  id: { control: 'text' },
-  name: { control: 'text' },
-  label: { control: 'text' },
-  validationType: { control: 'select', options: ['success', 'error', 'warning'] },
-  validationMessage: { control: 'text' },
-  icon: { control: 'text' },
-  hiddenLabel: {control: 'boolean', defaultValue: true},
 
-  }, 
+  argTypes: {
+    id: { control: 'text' },
+    name: { control: 'text' },
+    label: { control: 'text' },
+    placeholder: { control: 'text' },
+    validationType: {
+      control: 'select',
+      options: ['success', 'error', 'warning'],
+    },
+    validationMessage: { control: 'text' },
+    icon: { control: 'text' },
+    hiddenLabel: { control: 'boolean', defaultValue: true },
+  },
 } satisfies Meta<typeof InputGroup>
 
 export default meta
 type InputGroupStory = StoryObj<typeof InputGroup>
 
-// More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
 export const Default: InputGroupStory = {
   args: {
-    id: "inputGroup_id",
-    name: "inputGroup_name",
-  }, 
+    id: 'inputGroup_id',
+    name: 'inputGroup_name',
+    label: 'Default InputGroup',
+    placeholder: 'Write here',
+  },
 }
 
 export const WithValidationMessage: InputGroupStory = {
   args: {
-    id: "inputGroup_id",
-    name: "inputGroup_name",
-    validationMessage: "error message.",
-    validationType: "error",
-    
+    id: 'inputGroup_id',
+    name: 'inputGroup_name',
+    label: 'InputGroup with validation message',
+    placeholder: 'Write here',
+    validationMessage: 'error message.',
+    validationType: 'error',
   },
 }
-export const WithIcon : InputGroupStory = {
+export const WithIcon: InputGroupStory = {
   args: {
-    id: "inputGroup_id",
-    name: "inputGroup_name",
-    icon: "search",
+    id: 'inputGroup_id',
+    name: 'inputGroup_name',
+    label: 'InputGroup with icon',
+    placeholder: 'Write here',
+    icon: 'search',
   },
 }
