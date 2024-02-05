cbc31bdcd2e7c4f066cbe584d260c0dde7f05982
REALcuba <angelskcuba@gmail.com>
2024-02-05 08:49:10 +0100

    update

--- packages/ui/src/components/molecules/index.ts
+++ packages/ui/src/components/molecules/index.ts
@@ -1,6 +1,7 @@
 import { BackButton } from './BackButton'
 import { Tabs } from './Tabs'
+import { CreateAuthor } from './CreateAuthor'
 
 export { InputGroup } from './InputGroup'
 export { Modal } from './Modal'
-export { BackButton, Tabs }
+export { BackButton, Tabs, CreateAuthor }
