39953f8b3080388496b3fb2d9f05d3a81d787373
Natàlia <92027716+NataliaR-BCN@users.noreply.github.com>
2024-02-07 17:04:44 +0100

    951-fe-migrate-filter-widget-to-package-UI (#956)
    * 951-fe-migrate-filter-widget-to-package-ui
    
    * 951-fe-migrate-filter-widget-to-package-ui

--- packages/ui/CHANGELOG.md
+++ packages/ui/CHANGELOG.md
@@ -2,11 +2,17 @@
 
 All notable changes to this project will be documented in this file.
 
+## [0.19.0] - 2024-02-07
+
+### Added
+
+- Migrate CheckboxFilterWidget to UI
+
 ## [0.18.0] - 2024-02-06
 
 ### Added
 
-- Migrate CreateAuthor to UI.
+- Migrate CreateAuthor to UI
 
 ## [0.17.0] - 2024-02-06
 
--- packages/ui/src/components/molecules/index.ts
+++ packages/ui/src/components/molecules/index.ts
@@ -1,12 +1,16 @@
-
 export {
   AccessModalContent,
   type TAccessModalContent,
 } from './AccessModalContent'
 export { BackButton, type TBackButton } from './BackButton'
 export { CardProfile, type TCardProfile } from './CardProfile'
+export {
+  CheckboxFilterWidget,
+  type TCheckboxFilterItem,
+  type TCheckboxFilterWidget,
+} from './CheckboxFilterWidget'
+export { CreateAuthor, type TCreateAuthor } from './CreateAuthor'
 export { InputGroup, type TInputGroup } from './InputGroup'
 export { Modal, type TModal } from './Modal'
 export { SelectGroup, type TSelectGroup } from './SelectGroup'
 export { Tabs, type TTabs } from './Tabs'
-export { CreateAuthor, type TCreateAuthor } from './CreateAuthor'
