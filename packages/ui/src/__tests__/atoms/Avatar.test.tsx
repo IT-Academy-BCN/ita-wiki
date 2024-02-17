1e8b82368051b42338b3a97d2ef5b7c33e6ed793
REALcuba <angelskcuba@gmail.com>
2024-02-17 14:36:01 +0100

    update test

--- packages/ui/src/__tests__/atoms/Avatar.test.tsx
+++ packages/ui/src/__tests__/atoms/Avatar.test.tsx
@@ -9,6 +9,16 @@ const defaultAvatarCss = {
   borderRadius: '50%',
 }
 describe('Avatar', () => {
+  it('should render with default props', () => {
+    render(<Avatar src="/test.png" alt="users portrait" />)
+
+    const image: HTMLImageElement = screen.getByRole('img')
+
+    expect(image).toBeInTheDocument()
+    expect(image).toHaveAttribute('src', '/test.png')
+    expect(image).toHaveAttribute('alt', 'users portrait')
+
+  })
   it('should render with given props', () => {
     render(<Avatar src="/test.png" alt="users portrait" avatarCss={defaultAvatarCss} onClick={mockClick} />)
 
@@ -19,9 +29,11 @@ describe('Avatar', () => {
     expect(image).toHaveAttribute('alt', 'users portrait')
    
     expect(image).toHaveStyle('width: 97px; height: 97px; border-radius: 50%;');
+    expect(image).toHaveStyle('cursor: pointer;');
 
     fireEvent.click(image)
     expect(mockClick).toHaveBeenCalled()
 
   })
+
 })
