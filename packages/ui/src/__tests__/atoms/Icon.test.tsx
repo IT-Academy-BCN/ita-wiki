d4fa08d112e3057217879d676aeff894c790d28c
REALcuba <angelskcuba@gmail.com>
2024-02-22 09:13:56 +0100

    test updated

--- packages/ui/src/__tests__/atoms/Icon.test.tsx
+++ packages/ui/src/__tests__/atoms/Icon.test.tsx
@@ -24,24 +24,20 @@ describe('Icon', () => {
   })
 
   it('renders correctly with fill = 1', () => {
-    let isFavorite = false
-    const mockOnClick = vi.fn(() => {
-      isFavorite = true;
-    })
-    const updatedFill = isFavorite ? 1 : 0
+    const onClick = vi.fn();
+
     render(
-      <Icon data-testid="icon" name="favorite" onClick={mockOnClick} $fill={updatedFill}>
+      <Icon data-testid="icon" name="favorite" onClick={onClick} $fill={1}>
         favorite
       </Icon>
     )
     const icon = screen.getByTestId('icon')
     expect(icon).toBeInTheDocument();
     fireEvent.click(icon);
-    expect(mockOnClick).toHaveBeenCalled();
+    expect(onClick).toHaveBeenCalled();
     expect(icon).toHaveAttribute('name', 'favorite');
-    expect(isFavorite).toBe(true);
-    // expect(icon).toHaveStyle(
-    //   `font-variation-settings: 'FILL' 1, 'wght' 400,'GRAD' 0, 'opsz' 48;`
-    // )
+    expect(icon).toHaveStyle(
+      `font-variation-settings: 'FILL' 1, 'wght' 400,'GRAD' 0, 'opsz' 48;`
+    )
   })
 })
