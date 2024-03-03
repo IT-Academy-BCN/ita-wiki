276386b481b4bbff952a825ad1845546f84aa5b7
ursulacubilla <ursulacubilla@gmail.com>
2024-03-03 18:20:37 +0100

    Added Icon SelectGroup

--- packages/ui/src/components/molecules/SelectGroup.tsx
+++ packages/ui/src/components/molecules/SelectGroup.tsx
@@ -18,19 +18,29 @@ const SelectGroupStyled = styled.div`
   }
   width: 100%;
 `
+
+const Icon = styled.img`
+    width: 24px;
+    height: 24px;
+    top: 10px;
+    left: 12px;
+`
+
 export type TSelectGroup = {
   id: string
   name: string
   label: string
   hiddenLabel?: boolean
   validationMessage?: TValidationMessage['text']
+  iconSrc?: string
 } & TSelect
 
 export const SelectGroup = forwardRef<HTMLSelectElement, TSelectGroup>(
-  ({ label, id, name, hiddenLabel, validationMessage, ...rest }, ref) => (
+  ({ label, id, name, hiddenLabel, validationMessage, iconSrc, ...rest }, ref) => (
     <SelectGroupStyled>
       <Label text={label} htmlFor={id} hiddenLabel={hiddenLabel} />
       <Select id={id} name={name} ref={ref} {...rest} />
+      {iconSrc && <Icon src={iconSrc} />}
       <ValidationMessageStyled text={validationMessage} color="error" />
     </SelectGroupStyled>
   )
