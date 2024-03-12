a8979ee3d2da775fd9be49aeb171e349cc5c98f4
Ami <117275293+MiguelKummetz@users.noreply.github.com>
2024-03-12 11:38:32 +0100

    Update login.test.ts

--- services/sso/src/__tests__/auth/login.test.ts
+++ services/sso/src/__tests__/auth/login.test.ts
@@ -64,9 +64,9 @@ it('should fail if user not active', async () => {
 
 it('should fail if user is blocked', async () => {
   const response = await supertest(server).post(route).send({
-    dni: testUserData.inactiveUser.dni,
-    password: testUserData.inactiveUser.password,
+    dni: testUserData.blockedUser.dni,
+    password: testUserData.blockedUser.password,
   })
   expect(response.status).toBe(403)
-  expect(response.body.message).toBe('Only active users can login')
+  expect(response.body.message).toBe('The user is Blocked')
 })
