7c2f5e6a2e45444297c2fa283f9eecee5db25f34
0xNordian <85548493+0xNordian@users.noreply.github.com>
2024-03-13 14:12:36 +0100

    1030-fe-SideMenu links, with hover and active state added + component code refactored (#1056)
    SideMenu links, with hover and active state added + component code refactored

--- web/usuaris/src/__tests__/organisms/SideMenu.test.tsx
+++ web/usuaris/src/__tests__/organisms/SideMenu.test.tsx
@@ -1,12 +1,48 @@
-import { render, screen } from '@testing-library/react'
+import { fireEvent, screen } from '@testing-library/react'
+import { colors } from '@itacademy/ui'
+import { describe, beforeEach, it, expect } from 'vitest'
+import { render } from '../test-utils'
 import { SideMenu } from '../../components/organisms'
 
 describe('SideMenu', () => {
-  it('renders correctly', () => {
+  beforeEach(() => {
     render(<SideMenu />)
+  })
 
+  it('renders correctly', () => {
     expect(screen.getByAltText('IT Academy')).toBeInTheDocument()
-    expect(screen.getByText('Usuari@s')).toBeInTheDocument()
-    expect(screen.getByText('Mentores')).toHaveStyle('font-size: 0.875rem')
+    expect(screen.getByText('Usuaris')).toBeInTheDocument()
+    expect(screen.getByText('Mentors')).toBeInTheDocument()
+    expect(screen.getByText('Connector')).toBeInTheDocument()
+    expect(screen.getByText('Configuració')).toBeInTheDocument()
+  })
+
+  it('navigates to the correct page when a menu item is clicked', () => {
+    const firstMenuItem = screen.getByText('Usuaris')
+    fireEvent.click(firstMenuItem)
+    expect(window.location.pathname).toEqual('/')
+  })
+
+  it('changes style on hover over a category', () => {
+    const category = screen.getByText('Usuaris')
+    fireEvent.mouseOver(category)
+    expect(category).toHaveStyle(`color: ${colors.primary}`)
+  })
+
+  it('displays the correct icons for each menu item', () => {
+    const personIcons = screen.getAllByTestId('person')
+    expect(personIcons.length).toBe(2)
+
+    const boltIcon = screen.getByTestId('bolt')
+    expect(boltIcon).toBeInTheDocument()
+
+    const settingsIcon = screen.getByTestId('settings')
+    expect(settingsIcon).toBeInTheDocument()
+  })
+
+  it('ensures menu items are focusable for accessibility', () => {
+    const firstMenuItem = screen.getByText('Usuaris')
+    firstMenuItem.focus()
+    expect(firstMenuItem).toHaveFocus()
   })
 })
--- web/usuaris/src/components/organisms/SideMenu.tsx
+++ web/usuaris/src/components/organisms/SideMenu.tsx
@@ -1,23 +1,88 @@
 import { FC } from 'react'
+import { FlexBox, Icon, colors, dimensions, font } from '@itacademy/ui'
 import styled from 'styled-components'
-import { FlexBox, colors, dimensions, font } from '@itacademy/ui'
+import { Link } from 'react-router-dom'
+import { useTranslation } from 'react-i18next'
 import { icons } from '../../assets/icons'
+
+const LinkCategory = styled(Link)`
+  color: ${colors.gray.gray3};
+  font-size: ${font.xs};
+  font-weight: 600;
+  font-family: ${font.fontFamily};
+  cursor: pointer;
+  text-decoration: none;
+
+  &:hover {
+    color: ${colors.primary};
+  }
+`
+
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: auto 1fr;
+  gap: ${dimensions.spacing.xxxs};
+  align-items: center;
+`
 
 const SideMenuStyled = styled(FlexBox)`
   height: 100%;
+  min-width: 12rem;
   margin-top: ${dimensions.spacing.lg};
-  color: ${colors.black.black3};
 
   ul {
     list-style: none;
-    padding: ${dimensions.spacing.sm};
+    padding: ${dimensions.spacing.xxxs};
   }
 
   li {
     font-size: ${font.xs};
     font-weight: ${font.medium};
     margin-bottom: ${dimensions.spacing.xxl};
+
+    &::before {
+      font-size: larger;
+      color: ${colors.primary};
+      margin-right: 0.3rem;
+    }
   }
+`
+
+const CategoryStyled = styled.span`
+  color: ${colors.gray.gray3};
+  font-size: ${font.xs};
+  font-weight: 600;
+  font-family: ${font.fontFamily};
+  cursor: pointer;
+  transition: transform 0.3s ease;
+  outline: none;
+  display: inline-block;
+  border: 0px solid black;
+  tabindex: 0;
+
+  &:hover {
+    color: ${colors.primary};
+    transform: scale(1.05);
+  }
+
+  &:focus {
+    color: ${colors.black.black1};
+  }
+
+  &:focus::before {
+    content: '●';
+    font-size: larger;
+    color: ${colors.primary};
+    margin-right: 0.3rem;
+  }
+`
+
+const ImgStyled = styled(Icon)`
+  height: 30px;
+  margin-right: ${dimensions.spacing.xxxs};
+
+  &[data-testid] {
+    content: attr(data-testid);
 `
 
 const LogoImage = styled.img`
@@ -25,15 +90,56 @@ const LogoImage = styled.img`
   height: auto;
 `
 
-export const SideMenu: FC = () => (
-  <SideMenuStyled justify="space-between" align="start">
-    <LogoImage src={icons.itLogo} alt="IT Academy" />
-    <ul>
-      <li>Usuari@s</li>
-      <li>Mentores</li>
-      <li>Connector</li>
-      <li>Configuración</li>
-    </ul>
-    <div style={{ height: '9.4rem' }} />
-  </SideMenuStyled>
-)
+const menu = [
+  {
+    id: 1,
+    title: 'Usuarios',
+    icon: 'person',
+    link: '/#',
+  },
+  {
+    id: 2,
+    title: 'Mentores',
+    icon: 'person',
+    link: '/#',
+  },
+  {
+    id: 3,
+    title: 'Connector',
+    icon: 'bolt',
+    link: '/#',
+  },
+  {
+    id: 4,
+    title: 'Configuración',
+    icon: 'settings',
+    link: '/#',
+  },
+]
+
+export const SideMenu: FC = () => {
+  const { t } = useTranslation()
+
+  return (
+    <SideMenuStyled justify="space-between" align="start">
+      <LogoImage src={icons.itLogo} alt="IT Academy" />
+      <ul>
+        {menu.map((item) => (
+          <li key={item.id}>
+            <Grid>
+              <ImgStyled
+                data-testid={item.icon}
+                name={item.icon}
+                color={`${colors.black.black1}`}
+              />
+              <LinkCategory to={item.link} state={{}} key={1} tabIndex={0}>
+                <CategoryStyled tabIndex={0}>{t(item.title)}</CategoryStyled>
+              </LinkCategory>
+            </Grid>
+          </li>
+        ))}
+      </ul>
+      <div style={{ height: '79px' }} />
+    </SideMenuStyled>
+  )
+}
