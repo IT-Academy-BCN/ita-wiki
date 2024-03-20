92665376a63139264e8a4e86d1f8db917f4eb8f1
roche-tan <rochelyn.rallestan@gmail.com>
2024-03-20 19:16:32 +0100

    removed resources/topic from openapi and updated resources list controller

--- services/wiki/src/__tests__/resources/list.test.ts
+++ services/wiki/src/__tests__/resources/list.test.ts
@@ -7,7 +7,6 @@ import {
   Resource,
   User,
   ViewedResource,
-  Topic,
 } from '@prisma/client'
 import qs from 'qs'
 import { z } from 'zod'
@@ -33,15 +32,12 @@ let viewedResource: ViewedResource
 let user: User | null
 let adminUser: User | null
 let userWithNoName: User | null
-let testTopic: Topic
+// let testTopic: Topic
 
 beforeAll(async () => {
   const testCategory = (await prisma.category.findUnique({
     where: { slug: testCategoryData.slug },
   })) as Category
-  testTopic = (await prisma.topic.findUnique({
-    where: { slug: 'testing' },
-  })) as Topic
   user = await prisma.user.findFirst({
     where: { id: testUserData.user.id },
   })
@@ -125,9 +121,6 @@ afterAll(async () => {
   await prisma.resource.deleteMany({
     where: { user: { id: userWithNoName?.id } },
   })
-  // await prisma.topicsOnResources.deleteMany({
-  //   where: { topic: { id: testTopic.id } },
-  // })
 })
 // resourceTypes as array from prisma types for the it.each tests.
 const resourceTypes = Object.keys(RESOURCE_TYPE)
@@ -135,8 +128,6 @@ type ResourceGetSchema = z.infer<typeof resourceGetSchema>
 type TopicSchema = z.infer<typeof topicSchema>
 
 describe('Testing resources GET endpoint', () => {
-  const baseUrl = `${pathRoot.v1.resources}/topic`
-
   it('should fail with wrong resourceType', async () => {
     const response = await supertest(server)
       .get(`${pathRoot.v1.resources}`)
@@ -399,29 +390,6 @@ describe('Testing resources GET endpoint', () => {
       .query({ categorySlug: testCategoryData.slug, search })
     expect(response.status).toBe(200)
     expect(response.body.length).toBeGreaterThanOrEqual(3)
-  })
-
-  it('should respond with OK status if topic ID exists in database and return an array of resources associated with the topic ID  ', async () => {
-    const response = await supertest(server).get(`${baseUrl}/${testTopic.id}`)
-
-    expect(response.status).toBe(200)
-    expect(response.body.resources).toBeInstanceOf(Array)
-    expect(response.body.resources.length).toBeGreaterThan(0)
-    response.body.resources.forEach((resource: any) => {
-      expect(() => resourceGetSchema.parse(resource)).not.toThrow()
-      expect(
-        resource.topics.some((topic: any) => topic.topic.id === testTopic.id)
-      ).toBe(true)
-    })
-  })
-
-  it('should fail if topic ID does not exist in database ', async () => {
-    const response = await supertest(server).get(
-      `${baseUrl}/this-topicId-does-not-exist`
-    )
-
-    expect(response.status).toBe(404)
-    expect(response.body.message).toEqual('Topic not found')
   })
 
   checkInvalidToken(`${pathRoot.v1.resources}`, 'get')
--- services/wiki/src/controllers/resources/list.ts
+++ services/wiki/src/controllers/resources/list.ts
@@ -1,7 +1,6 @@
 import { Prisma, User } from '@prisma/client'
 import Koa, { Middleware } from 'koa'
 import { prisma } from '../../prisma/client'
-import { MissingParamError, NotFoundError } from '../../helpers/errors'
 import { resourceGetSchema } from '../../schemas'
 import { TResourcesListParamsSchema } from '../../schemas/resource/resourcesListParamsSchema'
 import {
@@ -74,41 +73,4 @@ export const listResources: Middleware = async (ctx: Koa.Context) => {
 
   ctx.status = 200
   ctx.body = parsedResources
-}
-
-export const getResourcesByTopicId: Middleware = async (ctx: Koa.Context) => {
-  const { topicId } = ctx.params
-
-  if (!topicId) throw new MissingParamError('topicId')
-
-  const topicFound = await prisma.topic.findUnique({
-    where: {
-      id: topicId,
-    },
-  })
-
-  if (!topicFound) throw new NotFoundError('Topic not found')
-
-  const resourcesList = await prisma.resource.findMany({
-    where: {
-      topics: {
-        some: {
-          topicId,
-        },
-      },
-    },
-    include: {
-      vote: { select: { vote: true } },
-      topics: { select: { topic: true } },
-    },
-  })
-
-  const resourcesWithUserName = await attachUserNamesToResources(resourcesList)
-  const parsedResources = resourcesWithUserName.map((resource) => {
-    const resourceWithVote = transformResourceToAPI(resource)
-    return resourceGetSchema.parse(resourceWithVote)
-  })
-
-  ctx.status = 200
-  ctx.body = { resources: parsedResources }
 }
