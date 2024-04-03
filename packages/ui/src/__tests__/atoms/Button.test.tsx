5154fe5c72fe4d29e91ac89122a380afe93c27d8
ursulacubilla <ursulacubilla@gmail.com>
2024-04-03 10:06:51 +0200

    ButtonTest

--- packages/ui/src/__tests__/atoms/Button.test.tsx
+++ packages/ui/src/__tests__/atoms/Button.test.tsx
@@ -1,4 +1,4 @@
-import { fireEvent, render, screen } from '@testing-library/react'
+import { fireEvent, render, screen, waitFor } from '@testing-library/react'
 import { vi } from 'vitest'
 import { dimensions, colors } from '../../styles'
 import {Button} from '../../components/atoms/Button'
@@ -6,21 +6,25 @@ import {Button} from '../../components/atoms/Button'
 const mockClick = vi.fn()
 
 describe('Button', () => {
-    it('renders correctly',async () => {
+    it('renders correctly', async () => {
         render(
-            <Button data-testid="button" onClick={mockClick} style={{backgroundColor: `${colors.primary}`}}>
+            <Button data-testid="button" onClick={mockClick}>
                 Test text
             </Button>
         )
+
         const button = screen.getByTestId('button')
-        
         expect(button).toBeInTheDocument()
-        expect(screen.getByText('Test text')).toBeInTheDocument()
-        expect(button).toHaveStyle(`border-radius: ${dimensions.borderRadius.base}`)
-        expect(button).toHaveStyle(`padding: ${dimensions.spacing.base}`)
-        expect(button).toHaveStyle(`background-color: ${colors.primary}`)
-        expect(button).toHaveStyle(`color: ${colors.white}`)
-        expect(button).toHaveStyle('cursor: pointer')
+
+        //NOTE: WaitFor is needed to wait for the styles to be applied
+        waitFor(() => {
+            expect(screen.getByText('Test text')).toBeInTheDocument()
+            expect(button).toHaveStyle(`border-radius: ${dimensions.borderRadius.base}`)
+            expect(button).toHaveStyle(`padding: ${dimensions.spacing.base}`)
+            expect(button).toHaveStyle(`background-color: ${colors.primary}`)
+            expect(button).toHaveStyle(`color: ${colors.white}`)
+            expect(button).toHaveStyle('cursor: pointer')
+        })
 
         fireEvent.click(button)
         expect(mockClick).toHaveBeenCalled()
@@ -28,27 +32,33 @@ describe('Button', () => {
 
     it('renders correctly with secondary', () => {
         render(
-            <Button data-testid="button" secondary style={{backgroundColor: `${colors.secondary}`, border: `2px solid ${colors.secondary}`}}>
+            <Button data-testid="button" secondary>
                 Test text
             </Button>
         )
         const button = screen.getByTestId('button')
 
-        expect(button).toHaveStyle(`background-color: ${colors.secondary}`)
-        expect(button).toHaveStyle(`border: 2px solid ${colors.secondary}`)
-        expect(button).toHaveStyle(`color: ${colors.white}`)
+        //NOTE: WaitFor is needed to wait for the styles to be applied
+        waitFor(() =>{
+            expect(button).toHaveStyle(`background-color: ${colors.secondary}`)
+            expect(button).toHaveStyle(`border: 2px solid ${colors.secondary}`)
+            expect(button).toHaveStyle(`color: ${colors.white}`)
+        })
     })
 
-    it('renders correctly with outline', () => {
+    it.only('renders correctly with outline', () => {
         render(
-            <Button data-testid="button" outline style={{backgroundColor: `${colors.white}`, border: `2px solid ${colors.gray.gray4}`}}>
+            <Button data-testid="button" outline>
                 Test text
             </Button>
         )
         const button = screen.getByTestId('button')
 
-        expect(button).toHaveStyle(`background-color: ${colors.white}`)
-        expect(button).toHaveStyle(`border: 2px solid ${colors.gray.gray4}`)
-        expect(button).toHaveStyle(`color: ${colors.gray.gray2}`)
+        //NOTE: WaitFor is needed to wait for the styles to be applied
+        waitFor(() =>{
+            expect(button).toHaveStyle(`background-color: ${colors.white}`)
+            expect(button).toHaveStyle(`border: 2px solid ${colors.gray.gray4}`)
+            expect(button).toHaveStyle(`color: ${colors.gray.gray2}`)
+        })
     })
 })
