46a7717753bf5b949cc2e2329f6d7463a1ffce32
Maria Morales Reina <124587546+marmorrei@users.noreply.github.com>
2024-04-15 11:39:39 +0200

    Add checkbox + delete button to UsersTable (#1159)

---
+++ web/usuaris/src/components/organisms/UsersTable/UsersTable.tsx
@@ -0,0 +1,243 @@
+/* eslint-disable react/no-unstable-nested-components */
+import { ChangeEvent, FC, useEffect, useState } from 'react'
+import { useTranslation } from 'react-i18next'
+import { ColumnDef, createColumnHelper } from '@tanstack/react-table'
+import { Checkbox, Spinner, dimensions } from '@itacademy/ui'
+import { Table } from '../../molecules'
+import { TUserData } from '../../../types'
+import { icons } from '../../../assets/icons'
+import { useGetUsers } from '../../../hooks'
+import {
+  ActionsContainer,
+  ActionsHeader,
+  ButtonStyled,
+  CellStyled,
+  DeleteButton,
+  DeleteIcon,
+  DisabledStyled,
+  IconStyled,
+  StatusStyled,
+  TableContainer,
+} from './UsersTable.styles'
+
+export const UsersTable: FC = () => {
+  const { t } = useTranslation()
+  const { isLoading, isError, data: users } = useGetUsers()
+  const [selectedStatus, setSelectedStatus] = useState<string | undefined>(
+    undefined
+  )
+  const [selectedUsers, setSelectedUsers] = useState<TUserData[]>([])
+
+  const handleSelectedUsers = (allSelectedUsers: TUserData[]) => {
+    // TO DO: Add action to do after selection
+    // eslint-disable-next-line no-console
+    console.log('allSelectedUsers', allSelectedUsers)
+  }
+
+  useEffect(() => {
+    if (selectedUsers?.length > 0) {
+      handleSelectedUsers(selectedUsers)
+    } else {
+      handleSelectedUsers([])
+      setSelectedStatus(undefined)
+    }
+  }, [selectedUsers])
+
+  const changeSelection = (
+    e: ChangeEvent<HTMLInputElement>,
+    id: string,
+    status: string
+  ) => {
+    if (e.target.checked) {
+      setSelectedStatus(status)
+      const userSelected: TUserData | undefined = users?.find(
+        (user) => user.id === id
+      )
+      const addUsers = [...selectedUsers]
+      if (userSelected) {
+        addUsers.push(userSelected)
+      }
+      setSelectedUsers(addUsers)
+
+      return addUsers
+    }
+
+    const userUnselected = users?.find((user) => user.id === id)
+    const removeUsers = selectedUsers.filter((user) => user !== userUnselected)
+    setSelectedUsers(removeUsers)
+
+    return removeUsers
+  }
+
+  const columHelper = createColumnHelper<TUserData>()
+
+  const columns: ColumnDef<TUserData, string>[] = [
+    columHelper.accessor('id', {
+      header: '',
+      cell: ({ row }) => {
+        const id: string = row.getValue('id')
+        const name: string = row.getValue('name')
+        const status: string = row.getValue('status')
+        let isDisabled: boolean | undefined
+
+        if (selectedStatus && selectedStatus !== status) {
+          isDisabled = true
+        } else {
+          isDisabled = undefined
+        }
+
+        return (
+          <Checkbox
+            id={id}
+            label={name}
+            hiddenLabel
+            onChange={(e) =>
+              handleSelectedUsers(changeSelection(e, id, status))
+            }
+            defaultChecked={selectedUsers?.some(
+              (checkedUser) => checkedUser.id === id
+            )}
+            disabled={isDisabled}
+          />
+        )
+      },
+    }),
+    columHelper.accessor('name', {
+      header: `${t('Nombre')}`,
+      cell: ({ row }) => {
+        const name: string = row.getValue('name')
+        const status: string = row.getValue('status')
+        let isDisabled: boolean | undefined
+
+        if (selectedStatus && selectedStatus !== status) {
+          isDisabled = true
+        } else {
+          isDisabled = undefined
+        }
+        return <DisabledStyled disabled={isDisabled}>{name}</DisabledStyled>
+      },
+    }),
+    columHelper.accessor('itineraryName', {
+      header: `${t('Especialización')}`,
+      cell: ({ row }) => {
+        const itineraryName: string = row.getValue('itineraryName')
+        const status: string = row.getValue('status')
+        let isDisabled: boolean | undefined
+
+        if (selectedStatus && selectedStatus !== status) {
+          isDisabled = true
+        } else {
+          isDisabled = undefined
+        }
+        return (
+          <DisabledStyled disabled={isDisabled}>{itineraryName}</DisabledStyled>
+        )
+      },
+    }),
+    columHelper.accessor('status', {
+      header: () => (
+        <CellStyled>
+          {t('Estado')}
+          <IconStyled src={icons.sortDown} alt="sort-down" />
+        </CellStyled>
+      ),
+      cell: ({ row }) => {
+        const status: string = row.getValue('status')
+        let isDisabled: boolean | undefined
+        let displayedStatus: string = ''
+
+        if (selectedStatus && selectedStatus !== status) {
+          isDisabled = true
+        } else {
+          isDisabled = undefined
+        }
+
+        if (status === 'PENDING') {
+          displayedStatus = t('pendiente')
+        } else if (status === 'BLOCKED') {
+          displayedStatus = t('bloqueado')
+        } else {
+          displayedStatus = t('activo')
+        }
+        return (
+          <DisabledStyled disabled={isDisabled}>
+            <StatusStyled status={status}>{displayedStatus}</StatusStyled>
+          </DisabledStyled>
+        )
+      },
+    }),
+    columHelper.accessor('createdAt', {
+      header: `${t('Fecha alta')}`,
+      cell: ({ row }) => {
+        const createdAt = row.getValue('createdAt')
+        const status: string = row.getValue('status')
+        const formattedDate = new Date(createdAt as string).toLocaleDateString()
+        let isDisabled: boolean | undefined
+
+        if (selectedStatus && selectedStatus !== status) {
+          isDisabled = true
+        } else {
+          isDisabled = undefined
+        }
+
+        return (
+          <DisabledStyled disabled={isDisabled}>{formattedDate}</DisabledStyled>
+        )
+      },
+    }),
+    columHelper.display({
+      id: 'actions',
+      header: () => (
+        <ActionsHeader align="end">
+          <CellStyled>{t('Acciones')}</CellStyled>
+        </ActionsHeader>
+      ),
+      cell: ({ row }) => {
+        const status: string = row.getValue('status')
+        let isDisabled: boolean | undefined
+        let buttonTxt: string = ''
+
+        if (selectedStatus && selectedStatus !== status) {
+          isDisabled = true
+        } else {
+          isDisabled = undefined
+        }
+
+        if (status === 'PENDING') {
+          buttonTxt = t('Aceptar')
+        } else if (status === 'BLOCKED') {
+          buttonTxt = t('Desbloquear')
+        } else {
+          buttonTxt = t('Bloquear')
+        }
+
+        return (
+          <ActionsContainer
+            direction="row"
+            justify="flex-end"
+            gap={dimensions.spacing.xs}
+          >
+            <ButtonStyled size="small" outline disabled={isDisabled}>
+              {buttonTxt}
+            </ButtonStyled>
+            <DeleteButton size="small" outline disabled={isDisabled}>
+              <DeleteIcon src={icons.deleteIcon} alt="delete-icon" />
+            </DeleteButton>
+          </ActionsContainer>
+        )
+      },
+    }),
+  ]
+
+  if (isLoading) return <Spinner size="medium" role="status" />
+  if (isError) return <p>{t('Ha habido un error...')}</p>
+  return (
+    <TableContainer>
+      <Table
+        columns={columns}
+        data={users || []}
+        noResultsMessage={t('No hay usuarios para mostrar')}
+      />
+    </TableContainer>
+  )
+}
