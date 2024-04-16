d1c85543959348509b6fad77bbb488362d6abf71
Ami <117275293+MiguelKummetz@users.noreply.github.com>
2024-04-16 12:54:35 +0200

    minor fix

--- services/sso/src/openapi/routes/dashboard/users/delete.ts
+++ services/sso/src/openapi/routes/dashboard/users/delete.ts
@@ -7,13 +7,17 @@ import {
   userNotFoundResponse,
 } from '../../../components/responses'
 import { registry } from '../../../registry'
+import { userIdSchema } from '../../../../schemas/users/userSchema'
 
 registry.registerPath({
   method: 'delete',
   tags: ['dashboard'],
   path: `${pathRoot.v1.dashboard.users}/{id}`,
   description: 'Soft deletes a user',
-  summary: 'delete a user',
+  summary: 'Delete a user',
+  request: {
+    params: z.object({ id: userIdSchema}),
+  },
   security: [{ [cookieAuth.name]: [] }],
   responses: {
     204: {
