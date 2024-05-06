5a687b02b6160e9c3a5b97ff096924c6acb9321b
Ami <117275293+MiguelKummetz@users.noreply.github.com>
2024-05-06 12:05:10 +0200

    1196-be-sso-refactor-dashboarduserslist-query (#1215)
    * added queryBuilder.ts to utils
    
    * version updated
    
    * lint fix
    
    * unit test moved to a new file

--- services/sso/src/controllers/dashboard/users/list.ts
+++ services/sso/src/controllers/dashboard/users/list.ts
@@ -1,73 +1,10 @@
 import { Context, Middleware } from 'koa'
 import { client } from '../../../models/db'
-import { itinerarySlugSchema } from '../../../schemas/itineraries/itinerarySchema'
-import {
-  userNameSchema,
-  userRoleSchema,
-  userStatusSchema,
-} from '../../../schemas/users/userSchema'
-import {
-  endDateSchema,
-  startDateSchema,
-} from '../../../schemas/users/dashboardUsersListQuerySchema'
-import { dniQueryStringSchema } from '../../../schemas/dniQueryStringSchema'
+import { queryBuilder } from '../../../utils/queryBuilder'
 
 export const dashboardListUsers: Middleware = async (ctx: Context) => {
-  const { itinerarySlug, status, startDate, endDate, name, dni, role } =
-    ctx.state.query
-  let query = `
-  SELECT
-    u.id,
-    u.name AS name,
-    u.dni AS dni,
-    u.status,
-    u.role,
-    TO_CHAR(u.created_at, 'YYYY-MM-DD') AS "createdAt",
-    i.name AS "itineraryName"
-  FROM
-    "user" u
-  JOIN itinerary i ON u.itinerary_id = i.id
-`
-  const queryParams = []
-  const conditions = []
-  if (name) {
-    const parsedName = userNameSchema.parse(name)
-    conditions.push(`u.name ILIKE $${conditions.length + 1}`)
-    queryParams.push(`%${parsedName}%`)
-  }
-  if (dni) {
-    const parsedDni = dniQueryStringSchema.parse(dni)
-    conditions.push(`u.dni ILIKE $${conditions.length + 1}`)
-    queryParams.push(`%${parsedDni}%`)
-  }
-  if (status) {
-    const parsedStatus = userStatusSchema.parse(status)
-    conditions.push(`u.status = $${conditions.length + 1}`)
-    queryParams.push(parsedStatus)
-  }
-  if (role) {
-    const parsedRole = userRoleSchema.parse(role)
-    conditions.push(`u.role = $${conditions.length + 1}`)
-    queryParams.push(parsedRole)
-  }
-  if (startDate) {
-    const parsedStartDate = startDateSchema.parse(startDate)
-    conditions.push(`u.created_at >= $${conditions.length + 1}`)
-    queryParams.push(parsedStartDate)
-  }
-  if (endDate) {
-    const parsedEndDate = endDateSchema.parse(endDate)
-    conditions.push(`u.created_at <= $${conditions.length + 1}`)
-    queryParams.push(parsedEndDate)
-  }
-  if (itinerarySlug) {
-    const parsedSlug = itinerarySlugSchema.parse(itinerarySlug)
-    conditions.push(`i.slug = $${conditions.length + 1}`)
-    queryParams.push(parsedSlug)
-  }
-  if (conditions.length > 0) {
-    query += ` WHERE ${conditions.join(' AND ')}`
-  }
+  const { query, queryParams } = queryBuilder(ctx.state.query)
+
   const queryResult = await client.query(query, queryParams)
 
   if (!queryResult.rowCount) {
