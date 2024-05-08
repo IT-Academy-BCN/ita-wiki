76dd040ef3eaa9d687fd5f82240d56d1151b9d04
thejbordo <joan.bordonaba@gmail.com>
2024-05-08 17:23:43 +0200

    ...

--- services/sso/src/__tests__/unit/queryBuilder.test.ts
+++ services/sso/src/__tests__/unit/queryBuilder.test.ts
@@ -9,6 +9,31 @@ const testStatus = UserStatus.ACTIVE
 const testDate = '2024-04-28T22:00:00.000Z'
 
 describe('Testing query builder function', () => {
+<<<<<<< HEAD
+=======
+  it('returns a expected query calling the queryBuilder function with name, dni, role, status and deletedAt', async () => {
+    const searchValues = {
+      name: testName,
+      dni: testDni,
+      role: testRole,
+      status: testStatus,
+      deletedAt: testDeletedAt,
+    }
+    const expectedWhere = `WHERE (u.name ILIKE $1 OR u.dni ILIKE $2) AND u.status = $3 AND u.role = $4 AND u.deleted_at = $5`
+    const expectedParams = [
+      '%testing%',
+      '%38826335N%',
+      'ACTIVE',
+      'REGISTERED',
+      'null',
+    ]
+    const resultQuery = queryBuilder(searchValues)
+    const resultWhere = resultQuery.query.substring(241)
+    console.log('asd', resultWhere)
+    expect(resultWhere).toBe(expectedWhere)
+    expect(resultQuery.queryParams).toEqual(expectedParams)
+  })
+>>>>>>> parent of d4f1e97 (deleted all console.log())
   it('returns a expected query calling the queryBuilder function with name, dni, role and status', async () => {
     const searchValues = {
       name: testName,
