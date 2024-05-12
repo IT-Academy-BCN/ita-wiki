eb02a7bd4b8d07c8e62c4fc9722752cc39a7bfac
thejbordo <joan.bordonaba@gmail.com>
2024-05-12 13:29:40 +0200

    Add test clear cookies and on endpoint too

--- services/wiki/src/__tests__/auth/logout.test.ts
+++ services/wiki/src/__tests__/auth/logout.test.ts
@@ -6,6 +6,10 @@ import { pathRoot } from '../../routes/routes'
 describe('Testing authentication endpoint', () => {
   test('should succeed', async () => {
     const response = await supertest(server).get(`${pathRoot.v1.auth}/logout`)
+    const cookies = response.header['set-cookie']
+
+    expect(cookies[1]).toMatch(/authToken=;/)
+    expect(cookies[2]).toMatch(/refreshToken=;/)
     expect(response.status).toBe(204)
   })
 })
--- services/wiki/src/controllers/auth/logoutController.ts
+++ services/wiki/src/controllers/auth/logoutController.ts
@@ -2,5 +2,13 @@ import { Context } from 'koa'
 
 export const logoutController = async (ctx: Context) => {
   ctx.cookies.set('token', null)
+  ctx.cookies.set('authToken', null, {
+    httpOnly: true,
+    maxAge: 0,
+  })
+  ctx.cookies.set('refreshToken', null, {
+    httpOnly: true,
+    maxAge: 0,
+  })
   ctx.status = 204
 }
