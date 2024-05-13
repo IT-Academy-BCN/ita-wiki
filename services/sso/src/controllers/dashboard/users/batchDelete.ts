92616565b3c9472b9814c34fd56ea0096f54ea27
Ami <117275293+MiguelKummetz@users.noreply.github.com>
2024-05-13 20:23:45 +0200

    batchDelete endpoint added

---
+++ services/sso/src/controllers/dashboard/users/batchDelete.ts
@@ -0,0 +1,38 @@
+import { Context, Middleware } from 'koa'
+import { client } from '../../../models/db'
+import { DeletedError, NotFoundError } from '../../../utils/errors'
+
+export const dashboardBatchDelete: Middleware = async (ctx: Context) => {
+  const { ids } = ctx.request.body
+  if (ids.length === 0) {
+    throw new NotFoundError('No user found')
+  }
+  const verifyQuery = `
+  SELECT id, deleted_at FROM "user" WHERE id = ANY($1::text[]);
+  `
+  const verifyResult = await client.query(verifyQuery, [ids])
+  const existingIds = verifyResult.rows.map((row) => row.id)
+  const notFoundIds = ids.filter((id: any) => !existingIds.includes(id))
+  if (notFoundIds.length > 0) {
+    throw new NotFoundError(`${notFoundIds} not found`)
+  }
+
+  let alreadyDeletedUser: string[] = []
+  const deletedAtList = verifyResult.rows.map((row) => row.deleted_at)
+  deletedAtList.map((deletedAt, index) => {
+    if (deletedAt !== null) {
+      alreadyDeletedUser.push(existingIds[index])
+    }
+  })
+  if (alreadyDeletedUser.length > 0) {
+    throw new DeletedError(`${alreadyDeletedUser} already deleted`)
+  }
+
+  const query = `
+  UPDATE "user"
+  SET deleted_at = CURRENT_TIMESTAMP
+  WHERE id = ANY($1::text[])
+  `
+  await client.query(query, [ids])
+  ctx.status = 204
+}
