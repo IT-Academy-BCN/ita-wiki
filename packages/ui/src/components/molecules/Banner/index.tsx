ee1636db81249887efb60d261698322f930c2a15
Maria Morales Reina <124587546+marmorrei@users.noreply.github.com>
2024-05-23 13:28:47 +0200

    Component styled

--- packages/ui/src/components/molecules/Banner/index.tsx
+++ packages/ui/src/components/molecules/Banner/index.tsx
@@ -5,18 +5,22 @@ import { FlexBox, colors, dimensions } from '../../../styles'
 import defaultImg from './defaultImg.svg'
 
 const BannerStyled = styled(FlexBox)`
-  width: 36.4rem;
-  height: 16.2rem;
+  width: 34.65rem;
+  height: 14.45rem;
   background-color: ${colors.primary};
-  padding: ${dimensions.spacing.sm};
   border-radius: ${dimensions.borderRadius.base};
+  padding: ${dimensions.spacing.lg};
 `
 
 const InfoStyled = styled(FlexBox)`
   width: 24.5rem;
 `
 
-const TitleStyled = styled(Title)``
+const TitleStyled = styled(Title)`
+  font-size: 32px;
+  margin-top: ${dimensions.spacing.xxxs};
+  margin-bottom: ${dimensions.spacing.xxxs};
+`
 
 const TextStyled = styled(Text)`
   color: ${colors.white};
@@ -25,15 +29,20 @@ const TextStyled = styled(Text)`
 const ButtonStyled = styled(Button)`
   background-color: #1e1e1e;
   width: 11rem;
+  font-weight: 700;
+  font-size: 16px;
 `
 
 const ImageContainer = styled(FlexBox)`
   width: 33%;
+  height: auto;
 `
 
 const ImageStyled = styled.img`
   width: 100%;
-  border-radius: ${dimensions.borderRadius.xs};
+  height: 100%;
+  border-radius: ${dimensions.borderRadius.base};
+  object-fit: cover;
 `
 
 export type TBanner = {
@@ -42,6 +51,7 @@ export type TBanner = {
   buttonText: string
   imgUrl?: string
   imgAltText?: string
+  onClick: () => void
 }
 
 export const Banner: FC<TBanner> = ({
@@ -50,14 +60,19 @@ export const Banner: FC<TBanner> = ({
   buttonText,
   imgUrl = defaultImg,
   imgAltText = 'e-book',
+  onClick,
 }) => (
-  <BannerStyled direction="row" justify="center" gap={dimensions.spacing.sm}>
-    <InfoStyled align="start">
+  <BannerStyled direction="row" align="stretch" gap={dimensions.spacing.sm}>
+    <InfoStyled
+      align="start"
+      justify="space-between"
+      gap={dimensions.spacing.xxs}
+    >
       <TitleStyled as="h1" fontWeight="bold" color={colors.white}>
         {title}
       </TitleStyled>
       <TextStyled fontSize="16px">{description}</TextStyled>
-      <ButtonStyled>{buttonText}</ButtonStyled>
+      <ButtonStyled onClick={onClick}>{buttonText}</ButtonStyled>
     </InfoStyled>
     <ImageContainer>
       <ImageStyled src={imgUrl} alt={imgAltText} />
