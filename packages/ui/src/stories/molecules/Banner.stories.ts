fcf8d792f5e6ccd907b8d8678517aa2bf0608afb
Maria Llenas <144248207+mllenas12@users.noreply.github.com>
2024-06-03 10:51:42 +0200

    Improve Banner molecule (#1312)

--- packages/ui/src/stories/molecules/Banner.stories.ts
+++ packages/ui/src/stories/molecules/Banner.stories.ts
@@ -9,6 +9,11 @@ const meta = {
   },
   tags: ['autodocs'],
   argTypes: {
+    id: { control: 'text' },
+    size: {
+      control: { type: 'select' },
+      options: ['small', 'normal'],
+    },
     title: { control: 'text' },
     description: { control: 'text' },
     buttonText: { control: 'text' },
@@ -23,20 +28,34 @@ type BannerStory = StoryObj<typeof Banner>
 
 export const Default: BannerStory = {
   args: {
+    id: '1',
     title: 'Sample Title',
     description: 'This is a sample description for the banner.',
     buttonText: 'Accept',
+    imgAltText: 'Alt image text',
   },
 }
 
 export const WithImg: BannerStory = {
   args: {
+    id: '2',
     title: 'Banner with image',
     description:
-      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry´s standard dummy text ever since the 1500s,',
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
     buttonText: 'Accept',
     imgUrl:
       'https://images.unsplash.com/photo-1601467295274-f2408b6e90f2?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    imgAltText: 'Alt image text',
+  },
+}
+
+export const Small: BannerStory = {
+  args: {
+    size: 'small',
+    id: '1',
+    title: 'Sample Title',
+    description: 'This is a sample description for the banner.',
+    buttonText: 'Accept',
     imgAltText: 'Alt image text',
   },
 }
