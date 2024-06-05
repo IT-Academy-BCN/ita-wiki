caafb8b25dcf52f55cfb989c9cfdf2edf85c9e5a
Cristian Moller <52261018+Cristian-Moller@users.noreply.github.com>
2024-06-05 08:53:07 +0200

    1319-feui-create-radiogroup-component (#1321)
    * Create RadioGroup Component
    
    * Fix Lint Error RadioGroup Component
    
    * Fix Lint Error2 RadioGroup Component
    
    * Fix Lint Error3 RadioGroup Component
    
    * Fix lint error
    
    * Fix RadioGroup
    
    * Fix RadioGroup 1

---
+++ packages/ui/src/__tests__/molecules/RadioGroup.test.tsx
@@ -0,0 +1,40 @@
+import { render, screen } from '@testing-library/react'
+import { RadioGroup } from '../../components/molecules'
+
+const mockRadioGroupProps = {
+  id: 'testid',
+  label: 'testid',
+  options: [
+    { id: 'test1', name: 'Test1' },
+    { id: 'test2', name: 'Test2' },
+    { id: 'test3', name: 'Test3' },
+  ],
+  direction: 'row',
+  inputName: 'test_name',
+  errorMessage: 'error',
+}
+
+describe('RadioGroupText', () => {
+  it('renders correctly', () => {
+    render(
+      <RadioGroup {...mockRadioGroupProps} direction='row' />
+    )
+    expect(screen.getByText('testid')).toBeInTheDocument()
+  })
+
+  it('displays all options', () => {
+    render(
+      <RadioGroup {...mockRadioGroupProps} direction='row' />
+    );
+    mockRadioGroupProps.options.forEach(option => {
+      expect(screen.getByLabelText(option.name)).toBeInTheDocument();
+    });
+  });
+
+  it('renders correctly with error message', () => {
+    render(
+      <RadioGroup {...mockRadioGroupProps} direction='row' error />
+    )
+    expect(screen.getByText('error')).toBeInTheDocument()    
+  })
+})
