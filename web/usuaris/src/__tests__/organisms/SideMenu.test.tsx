67e2222d2ffc3a617457b1e49a107422637019fe
zookatt <139338863+zookatt@users.noreply.github.com>
2024-06-12 12:45:36 +0200

    1309-feusuaris-display-sidemenu-options-according-to-user-role (#1333)
    * 1309-feusuaris-display-sidemenu-options-according-to-user-role
    
    * error SideMenu fixed
    
    * test SideMenu fixed
    
    * SideMenu refactored, tests fixed
    
    * tests fixed
    
    * exports types fixed

--- web/usuaris/src/__tests__/organisms/SideMenu.test.tsx
+++ web/usuaris/src/__tests__/organisms/SideMenu.test.tsx
@@ -4,27 +4,44 @@ import { colors } from '@itacademy/ui'
 import { render } from '../test-utils'
 import { SideMenu } from '../../components/organisms'
 import { paths } from '../../constants'
+import { TAuthContext, useAuth } from '../../context/AuthProvider'
+import { UserRole } from '../../types'
 
 describe('SideMenu', () => {
   beforeEach(() => {
-    render(<SideMenu />)
+    vi.mock('../../context/AuthProvider', async () => {
+      const actual = await vi.importActual('../../context/AuthProvider')
+      return {
+        ...actual,
+        useAuth: vi.fn(),
+      }
+    })
   })
 
   it('renders correctly', async () => {
+    vi.mocked(useAuth).mockReturnValue({
+      user: {
+        dni: '12345678A',
+        email: 'user@example.cat',
+        role: UserRole.ADMIN,
+      },
+    } as TAuthContext)
+    render(<SideMenu />)
+
     expect(screen.getByAltText('IT Academy')).toBeInTheDocument()
-    const usersItem = screen.getByTestId('test-title-Usuarios')
-    const mentorsItem = screen.getByTestId('test-title-Mentores')
-    const connectorItem = screen.getByTestId('test-title-Connector')
-    const settingsItem = screen.getByTestId('test-title-Configuración')
+    const usersItem = screen.getByTestId('menu-title-usuarios')
+    const mentorsItem = screen.getByTestId('menu-title-mentores')
+    const connectorItem = screen.getByTestId('menu-title-connector')
+    const settingsItem = screen.getByTestId('menu-title-configuración')
 
     expect(usersItem).toBeInTheDocument()
     expect(mentorsItem).toBeInTheDocument()
     expect(connectorItem).toBeInTheDocument()
     expect(settingsItem).toBeInTheDocument()
 
-    expect(screen.getAllByTestId('test-icon-person').length).toBe(2)
-    expect(screen.getByTestId('test-icon-bolt')).toBeInTheDocument()
-    expect(screen.getByTestId('test-icon-settings')).toBeInTheDocument()
+    expect(screen.getAllByTestId('menu-icon-person').length).toBe(2)
+    expect(screen.getByTestId('menu-icon-bolt')).toBeInTheDocument()
+    expect(screen.getByTestId('menu-icon-settings')).toBeInTheDocument()
 
     waitFor(() => {
       expect(usersItem).toHaveStyle(`color: ${colors.black.black3}`)
@@ -35,23 +52,39 @@ describe('SideMenu', () => {
   })
 
   it('navigates to the correct page when a menu item is clicked and changes styles', async () => {
-    const mentorsMenuItemLink = screen.getByTestId('test-link-Mentores')
-    const mentorsTitle = screen.getByTestId('test-title-Mentores')
+    vi.mocked(useAuth).mockReturnValue({
+      user: {
+        dni: '12345678A',
+        email: 'user@example.cat',
+        role: UserRole.ADMIN,
+      },
+    } as TAuthContext)
+    render(<SideMenu />)
+    const mentorsMenuItemLink = screen.getByTestId('menu-link-mentores')
+    const mentorsTitle = screen.getByTestId('menu-title-mentores')
 
-    expect(screen.getByTestId(`test-path-${paths.home}`)).toBeInTheDocument()
+    expect(screen.getByTestId(`menu-path-${paths.home}`)).toBeInTheDocument()
     waitFor(() => {
       expect(mentorsTitle).toHaveStyle(`color: ${colors.gray.gray3}`)
     })
 
     await userEvent.click(mentorsMenuItemLink)
 
-    expect(screen.getByTestId(`test-path-${paths.mentors}`)).toBeInTheDocument()
+    expect(screen.getByTestId(`menu-path-${paths.mentors}`)).toBeInTheDocument()
     waitFor(() => {
       expect(mentorsTitle).toHaveStyle(`color: ${colors.black.black3}`)
     })
   })
 
   it('changes style on hover over a category', () => {
+    vi.mocked(useAuth).mockReturnValue({
+      user: {
+        dni: '12345678A',
+        email: 'user@example.cat',
+        role: UserRole.ADMIN,
+      },
+    } as TAuthContext)
+    render(<SideMenu />)
     const activeCategory = screen.getByText('Usuaris')
     fireEvent.mouseOver(activeCategory)
     expect(activeCategory).toHaveStyle(`color: ${colors.black.black3}`)
@@ -62,8 +95,44 @@ describe('SideMenu', () => {
   })
 
   it('ensures menu items are focusable for accessibility', () => {
-    const usersMenuItem = screen.getByTestId('test-link-Usuarios')
+    vi.mocked(useAuth).mockReturnValue({
+      user: {
+        dni: '12345678A',
+        email: 'user@example.cat',
+        role: UserRole.ADMIN,
+      },
+    } as TAuthContext)
+    render(<SideMenu />)
+    const usersMenuItem = screen.getByTestId('menu-link-usuarios')
     usersMenuItem.focus()
     expect(usersMenuItem).toHaveFocus()
+  })
+
+  it('renders Mentors option for admin user', () => {
+    vi.mocked(useAuth).mockReturnValue({
+      user: {
+        dni: '12345678A',
+        email: 'user@example.cat',
+        role: UserRole.ADMIN,
+      },
+    } as TAuthContext)
+    render(<SideMenu />)
+
+    const mentorsItem = screen.getByTestId('menu-title-mentores')
+    expect(mentorsItem).toBeInTheDocument()
+  })
+
+  it('does not render Mentors option for mentor user', () => {
+    vi.mocked(useAuth).mockReturnValue({
+      user: {
+        dni: '12345678A',
+        email: 'user@example.cat',
+        role: UserRole.MENTOR,
+      },
+    } as TAuthContext)
+    render(<SideMenu />)
+
+    const mentorsItem = screen.queryByTestId('menu-title-mentores')
+    expect(mentorsItem).not.toBeInTheDocument()
   })
 })
