31242148cd4433ce8115aa7734f3c2485c4f487b
Cristian Moller <52261018+Cristian-Moller@users.noreply.github.com>
2024-06-20 09:02:47 +0200

    1337-feusuaris-disable-role-itinerary-and-delete-option-for-mentor-users (#1343)
    * 1337-feusuaris-disable-role-itinerary-and-delete-option-for-mentor-users
    
    * Delete styles
    
    * Refactor
    
    * Package update
    
    * Test ActionsDropdown
    
    * Update package
    
    * fix coverage
    
    * Fix coverage 1
    
    * fix lint
    
    * Fix lint 1
    
    * Fix test comments, change of languages in RoleFilter, remove delete button from table for role mentor
    
    * fix lint
    
    * fix test
    
    * fix test1
    
    * Fix test UsersTable
    
    * Fix bugs

--- web/usuaris/src/components/molecules/ActionsDropdown.tsx
+++ web/usuaris/src/components/molecules/ActionsDropdown.tsx
@@ -8,7 +8,8 @@ import {
   type TDropdownOption,
 } from '@itacademy/ui'
 import { useTranslation } from 'react-i18next'
-import { UserStatus } from '../../types'
+import { UserRole, UserStatus } from '../../types'
+import { useAuth } from '../../context/AuthProvider'
 
 const StyledDropdown = styled(Dropdown)`
   && button {
@@ -43,31 +44,50 @@ export const ActionsDropdown: FC<TActionsDropdown> = ({
   const { t } = useTranslation()
 
   const [actionsList, setActionsList] = useState<TDropdownOption[]>([])
+  const { user } = useAuth()
 
   useEffect(() => {
     switch (selectedStatus) {
       case UserStatus.ACTIVE:
-        setActionsList([
-          { id: UserStatus.BLOCKED, name: t('Bloquear'), icon: 'block' },
-          { id: 'DELETE', name: t('Eliminar'), icon: 'delete_forever' },
-        ])
+        if (user?.role === UserRole.ADMIN) {
+          setActionsList([
+            { id: UserStatus.BLOCKED, name: t('Bloquear'), icon: 'block' },
+            { id: 'DELETE', name: t('Eliminar'), icon: 'delete_forever' },
+          ])
+        } else if (user?.role === UserRole.MENTOR) {
+          setActionsList([
+            { id: UserStatus.BLOCKED, name: t('Bloquear'), icon: 'block' },
+          ])
+        }
         break
       case UserStatus.PENDING:
-        setActionsList([
-          { id: UserStatus.ACTIVE, name: t('Aceptar'), icon: 'task_alt' },
-          { id: 'DELETE', name: t('Eliminar'), icon: 'delete_forever' },
-        ])
+        if (user?.role === UserRole.ADMIN) {
+          setActionsList([
+            { id: UserStatus.ACTIVE, name: t('Aceptar'), icon: 'task_alt' },
+            { id: 'DELETE', name: t('Eliminar'), icon: 'delete_forever' },
+          ])
+        } else if (user?.role === UserRole.MENTOR) {
+          setActionsList([
+            { id: UserStatus.ACTIVE, name: t('Aceptar'), icon: 'task_alt' },
+          ])
+        }
         break
       case UserStatus.BLOCKED:
-        setActionsList([
-          { id: UserStatus.ACTIVE, name: t('Desbloquear'), icon: 'task_alt' },
-          { id: 'DELETE', name: t('Eliminar'), icon: 'delete_forever' },
-        ])
+        if (user?.role === UserRole.ADMIN) {
+          setActionsList([
+            { id: UserStatus.ACTIVE, name: t('Desbloquear'), icon: 'task_alt' },
+            { id: 'DELETE', name: t('Eliminar'), icon: 'delete_forever' },
+          ])
+        } else if (user?.role === UserRole.MENTOR) {
+          setActionsList([
+            { id: UserStatus.ACTIVE, name: t('Desbloquear'), icon: 'task_alt' },
+          ])
+        }
         break
       default:
         setActionsList([])
     }
-  }, [selectedStatus, t])
+  }, [selectedStatus, t, user])
 
   const handleSelectedValue = (selectedOption: TDropdownOption | undefined) => {
     handleAction(selectedOption?.id)
