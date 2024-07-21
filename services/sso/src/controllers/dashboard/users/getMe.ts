84062deb70f70ca86d4d1e811ba768bc3113db05
thejbordo <joan.bordonaba@gmail.com>
2024-07-21 23:36:00 +0200

    feat: migrate to knex getME endpoint and its tests.

--- services/sso/src/controllers/dashboard/users/getMe.ts
+++ services/sso/src/controllers/dashboard/users/getMe.ts
@@ -1,19 +1,21 @@
 import { Context, Middleware } from 'koa'
 import { User } from '../../../schemas'
-import { client } from '../../../db/client'
 import { NotFoundError } from '../../../utils/errors'
+import db from '../../../db/knexClient'
 
 export const getMe: Middleware = async (ctx: Context) => {
   const { id } = ctx.state.user as Pick<User, 'id' | 'role'>
-  const queryResult = await client.query(
-    'SELECT dni, email, name, role FROM "user" WHERE id = $1',
-    [id]
-  )
 
-  const user = queryResult.rows[0]
+  const queryResult = await db
+    .select('dni', 'email', 'name', 'role')
+    .from('user')
+    .where({ id })
+
+  const user = queryResult[0]
   if (!user) {
     throw new NotFoundError('User Not found')
   }
+
   ctx.status = 200
   ctx.body = user
 }
