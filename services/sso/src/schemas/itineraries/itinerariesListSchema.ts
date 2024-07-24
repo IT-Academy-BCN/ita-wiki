9d89acec076b1be5fd72879369db043ffa9c332f
Joan Bordonaba <joan.bordonaba@gmail.com>
2024-07-25 00:41:15 +0200

    1429-besso-migrate-to-knex-get-itinerarieslist-endpoints (#1444)
    * feat: migrate listItineraries endpoint to knex
    
    * Update list.test.ts

--- services/sso/src/schemas/itineraries/itinerariesListSchema.ts
+++ services/sso/src/schemas/itineraries/itinerariesListSchema.ts
@@ -2,3 +2,5 @@ import { z } from '../../openapi/zod'
 import { itinerarySchema } from './itinerarySchema'
 
 export const itinerariesListSchema = z.array(itinerarySchema)
+
+export type ItinerayList = z.infer<typeof itinerarySchema>
