52495c6c37f261518e200c693300a33e499fc974
Glòria Monzó <dark.aredhel@gmail.com>
2024-08-06 17:38:07 +0200

    1432-besso-migrate-to-knex-get-dashboardusers-endpoints

--- services/sso/src/controllers/dashboard/users/batchDelete.ts
+++ services/sso/src/controllers/dashboard/users/batchDelete.ts
@@ -1,34 +1,35 @@
 import { Context, Middleware } from 'koa'
 import { NotFoundError } from '../../../utils/errors'
-import { userManager } from '../../../db/managers/userManager'
-import db from '../../../db/knexClient'
+import { client } from '../../../db/client'
 
 export const dashboardBatchDelete: Middleware = async (ctx: Context) => {
   const { ids } = ctx.request.body
-
   if (ids.length === 0) {
     throw new NotFoundError('No user found')
   }
-
-  const users = await userManager.getUsersByIds(
-    { fields: ['id', 'deletedAt'] },
-    false,
-    ids
-  )
-
-  if (users.length === 0) {
+  const verifyQuery = `
+  SELECT id, deleted_at FROM "user" WHERE id = ANY($1::text[]);
+  `
+  const verifyResult = await client.query(verifyQuery, [ids])
+  const existingIds = verifyResult.rows.map((row) => row.id)
+  if (existingIds.length === 0) {
     throw new NotFoundError('No user found')
   }
 
-  const toBeDeletedUser = users
-    .filter((user) => user.deletedAt === null)
-    .map((user) => user.id)
+  const toBeDeletedUser: string[] = []
+  verifyResult.rows.forEach((row) => {
+    if (row.deleted_at === null) {
+      toBeDeletedUser.push(row.id)
+    }
+  })
 
   if (toBeDeletedUser.length > 0) {
-    await db('user')
-      .whereIn('id', toBeDeletedUser)
-      .update({ deleted_at: db.fn.now() })
+    const deleteQuery = `
+      UPDATE "user"
+      SET deleted_at = CURRENT_TIMESTAMP
+      WHERE id = ANY($1::text[])
+    `
+    await client.query(deleteQuery, [toBeDeletedUser])
   }
-
   ctx.status = 204
 }
