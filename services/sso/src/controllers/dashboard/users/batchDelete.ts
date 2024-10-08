7f1bf42feb8808bd66d35da3f05b511c219f15f5
Glòria Monzó <dark.aredhel@gmail.com>
2024-10-08 13:33:30 +0200

    1489-besso-migrate-to-knex-delete-dashboardusersbatchdelete-endpoints

--- services/sso/src/controllers/dashboard/users/batchDelete.ts
+++ services/sso/src/controllers/dashboard/users/batchDelete.ts
@@ -8,7 +8,6 @@ export const dashboardBatchDelete: Middleware = async (ctx: Context) => {
     throw new NotFoundError('No user found')
   }
 
-  // Verificar quins usuaris existeixen a la base de dades
   const verifyResult = await db('user')
     .select('id', 'deleted_at')
     .whereIn('id', ids)
@@ -19,18 +18,15 @@ export const dashboardBatchDelete: Middleware = async (ctx: Context) => {
     throw new NotFoundError('No user found')
   }
 
-  // Filtrar els usuaris que no han estat esborrats
   const toBeDeletedUser: string[] = verifyResult
     .filter((row) => row.deleted_at === null)
     .map((row) => row.id)
 
-  // Si hi ha usuaris per esborrar, actualitzar la seva columna 'deleted_at'
   if (toBeDeletedUser.length > 0) {
     await db('user')
       .whereIn('id', toBeDeletedUser)
       .update({ deleted_at: db.fn.now() })
   }
 
-  // Resposta 204: No Content, perquè no hi ha cos a la resposta
   ctx.status = 204
 }
