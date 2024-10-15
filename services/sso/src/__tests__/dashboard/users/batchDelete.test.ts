5de818c6c087ed776deb12003cda51dfa0994e78
Glòria Monzó <52612900+Aredhel269@users.noreply.github.com>
2024-10-15 20:44:27 +0200

    1495-besso-updated-tests-dashboardusersbatchDelete-to-Knex. (#1496)
    * Updated `dashboard/users/batchDelete.test ` to Knex.
    
    * upload change version

--- services/sso/src/__tests__/dashboard/users/batchDelete.test.ts
+++ services/sso/src/__tests__/dashboard/users/batchDelete.test.ts
@@ -2,7 +2,6 @@ import supertest from 'supertest'
 import { expect, it, describe, beforeEach, afterEach } from 'vitest'
 import { pathRoot } from '../../../routes/routes'
 import { server, testUserData } from '../../globalSetup'
-import { client } from '../../../db/client'
 import db from '../../../db/knexClient'
 
 const route = `${pathRoot.v1.dashboard.users}`
@@ -67,42 +66,40 @@ describe('Testing dashboard delete endpoint', () => {
     expect(response.body.message).toBe('No user found')
   })
   it('should succeed if the same data is provided twice but keeping the first deletedAt value', async () => {
-    let deletedAt1 = await client.query(
-      'SELECT deleted_at FROM "user" WHERE id IN ( $1, $2 )',
-      [userToDeleteId, userToBeBlockedId]
-    )
-    expect(deletedAt1.rows[0].deleted_at).toBe(null)
-    expect(deletedAt1.rows[1].deleted_at).toBe(null)
+    let deletedAt1 = await db('user')
+      .select('deleted_at')
+      .whereIn('id', [userToDeleteId, userToBeBlockedId])
+    expect(deletedAt1[0].deleted_at).toBe(null)
+    expect(deletedAt1[1].deleted_at).toBe(null)
     const ids = [userToDeleteId, userToBeBlockedId]
     const response1 = await supertest(server)
       .delete(`${route}/`)
       .set('Cookie', [authAdminToken])
       .send({ ids })
     expect(response1.status).toBe(204)
-    deletedAt1 = await client.query(
-      'SELECT deleted_at FROM "user" WHERE id IN ( $1, $2 )',
-      [userToDeleteId, userToBeBlockedId]
-    )
-    expect(deletedAt1.rows[0].deleted_at).toContain(Date)
-    expect(deletedAt1.rows[1].deleted_at).toContain(Date)
+    deletedAt1 = await db('user')
+      .select('deleted_at')
+      .whereIn('id', [userToDeleteId, userToBeBlockedId])
+
+    expect(deletedAt1[0].deleted_at).toContain(Date)
+    expect(deletedAt1[1].deleted_at).toContain(Date)
     const response2 = await supertest(server)
       .delete(`${route}/`)
       .set('Cookie', [authAdminToken])
       .send({ ids })
     expect(response2.status).toBe(204)
-    const deletedAt2 = await client.query(
-      'SELECT deleted_at FROM "user" WHERE id IN ( $1, $2 )',
-      [userToDeleteId, userToBeBlockedId]
-    )
-    expect(deletedAt1.rows[0].deleted_at).toEqual(deletedAt2.rows[0].deleted_at)
-    expect(deletedAt1.rows[1].deleted_at).toEqual(deletedAt2.rows[1].deleted_at)
+    const deletedAt2 = await db('user')
+      .select('deleted_at')
+      .whereIn('id', [userToDeleteId, userToBeBlockedId])
+    expect(deletedAt1[0].deleted_at).toEqual(deletedAt2[0].deleted_at)
+    expect(deletedAt1[1].deleted_at).toEqual(deletedAt2[1].deleted_at)
   })
   it('should succeed if one of the provided ids exist', async () => {
-    let deletedAt = await client.query(
-      'SELECT deleted_at FROM "user" WHERE id IN ( $1 )',
-      [userToDeleteId]
-    )
-    expect(deletedAt.rows[0].deleted_at).toBe(null)
+    let deletedAt = await db('user')
+      .select('deleted_at')
+      .whereIn('id', [userToDeleteId])
+
+    expect(deletedAt[0].deleted_at).toBe(null)
     const nonExistingId1 = 'ydmicpzwt2ss8zoo0o9oavax'
     const nonExistingId2 = 'q81ky22cpayhprqmn0a2slmn'
     const ids = [userToDeleteId, nonExistingId1, nonExistingId2]
@@ -111,10 +108,10 @@ describe('Testing dashboard delete endpoint', () => {
       .set('Cookie', [authAdminToken])
       .send({ ids })
     expect(response.status).toBe(204)
-    deletedAt = await client.query(
-      'SELECT deleted_at FROM "user" WHERE id IN ( $1 )',
-      [userToDeleteId]
-    )
-    expect(deletedAt.rows[0].deleted_at).toContain(Date)
+    deletedAt = await db('user')
+      .select('deleted_at')
+      .whereIn('id', [userToDeleteId])
+
+    expect(deletedAt[0].deleted_at).toContain(Date)
   })
 })
