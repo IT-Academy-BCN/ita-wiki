5c7fecd7da6ad5c327366af128a7033c9da5e5d8
Glòria Monzó <52612900+Aredhel269@users.noreply.github.com>
2024-10-22 09:05:39 +0200

    Migrate to Knex updateStatus (#1521)

--- services/sso/src/controllers/dashboard/users/updateStatus.ts
+++ services/sso/src/controllers/dashboard/users/updateStatus.ts
@@ -1,29 +1,22 @@
 import { Context, Middleware } from 'koa'
-import { client } from '../../../db/client'
+import db from '../../../db/knexClient'
 import { NotFoundError } from '../../../utils/errors'
 import { TDashboardUsersUpdateStatus } from '../../../schemas/users/dashboardUsersUpdateStatusSchema'
 
 export const dashboardUpdateStatusUsers: Middleware = async (ctx: Context) => {
   const { ids, status } = ctx.request.body as TDashboardUsersUpdateStatus
 
-  const verifyQuery = `
-      SELECT id FROM "user" WHERE id = ANY($1::text[]);
-    `
-  const verifyResult = await client.query(verifyQuery, [ids])
-  const existingIds = verifyResult.rows.map((row) => row.id)
+  const verifyResult = await db('user').select('id').whereIn('id', ids)
+
+  const existingIds = verifyResult.map((row) => row.id)
 
   const notFoundIds = ids.filter((id) => !existingIds.includes(id))
 
   if (notFoundIds.length > 0) {
     throw new NotFoundError(`${notFoundIds} not found`)
   }
-  const query = `
-    UPDATE "user"
-    SET status = $1
-    WHERE id = ANY($2::text[])
-    `
 
-  await client.query(query, [status, ids])
+  await db('user').update({ status }).whereIn('id', ids)
 
   ctx.status = 204
 }
