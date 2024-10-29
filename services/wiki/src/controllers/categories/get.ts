14b182d9f34dfe0ecca1ffc521ee528f4056abce
ClaudioMartinH <160405164+ClaudioMartinH@users.noreply.github.com>
2024-10-29 18:35:00 +0100

    updated getCategories controller to knex methods

--- services/wiki/src/controllers/categories/get.ts
+++ services/wiki/src/controllers/categories/get.ts
@@ -1,14 +1,8 @@
 import Koa, { Middleware } from 'koa'
-import { prisma } from '../../prisma/client'
+import db from '../../db/knex'
 
 export const getCategories: Middleware = async (ctx: Koa.Context) => {
-  const categories = await prisma.category.findMany({
-    select: {
-      id: true,
-      name: true,
-      slug: true,
-    },
-  })
+  const categories = await db('category').select('id', 'name', 'slug')
   ctx.status = 200
   ctx.body = categories
 }
